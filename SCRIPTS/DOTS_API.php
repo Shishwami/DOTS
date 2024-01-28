e2fe998e212ca3c89c067c44ec5ca38ee6315064
Shishwami <157940784+Shishwami@users.noreply.github.com>
2024-01-28 15:52:10 +0800

    Update DOTS_API.php

---
+++ SCRIPTS/DOTS_API.php
@@ -0,0 +1,64 @@
+<?php
+include "DB_Connect.php";
+include "Querries.php";
+include "Formatter.php";
+include "Constants.php";
+
+$inputs = json_decode(file_get_contents("php://input"), true);
+$REQUEST = $inputs['REQUEST'];
+unset($inputs['REQUEST']);
+switch ($REQUEST) {
+    case 'INSERT':
+        INSERT($inputs, $conn);
+        break;
+    case 'SELECT':
+        SELECT($inputs, $conn);
+        break;
+}
+$conn->close();
+
+
+function INSERT($inputs, $conn)
+{
+    $querries = new Querries();
+    $valid = false;
+
+    $TABLE_NAME = $inputs['TABLE_NAME'];
+    unset($inputs['TABLE_NAME']);
+
+    $sql = $querries->insertQuerry($TABLE_NAME, $inputs);
+
+    if (mysqli_query($conn, $sql)) {
+        $valid = true;
+    }
+
+    echo json_encode(
+        array(
+            'VALID' => $valid
+        )
+    );
+}
+
+function SELECT($inputs, $conn)
+{
+    $querries = new Querries();
+    $valid = false;
+
+    $TABLE_NAME = $inputs['TABLE_NAME'];
+    unset($inputs['TABLE_NAME']);
+
+    $sql = $querries->insertQuerry($TABLE_NAME, $inputs);
+
+    if (mysqli_query($conn, $sql)) {
+        $valid = true;
+    }
+
+    echo json_encode(
+        array(
+            'VALID' => $valid
+        )
+    );
+}
+
+
+?>
