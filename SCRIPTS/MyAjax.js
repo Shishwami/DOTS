bb0ac25113852c4dc12c7b58e3aadf7a2834d080
Shishwami <157940784+Shishwami@users.noreply.github.com>
2024-01-28 13:43:14 +0800

    Update MyAjax.js

---
+++ SCRIPTS/MyAjax.js
@@ -0,0 +1,21 @@
+class MyAjax {
+    static createJSON(callback,data) {
+        const xhr = new XMLHttpRequest();
+        xhr.open('POST', "../SCRIPTS/DOTS_API.php", true);
+
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState == 4) {
+                if (xhr.status == 200) {
+                    const response = JSON.parse(xhr.responseText);
+                    callback(null, response);
+                } else {
+                    callback(`Error: ${xhr.status}`);
+                }
+            }
+        };
+
+        const jsonData = JSON.stringify(data);
+        xhr.send(jsonData);
+    }
+}
+export default MyAjax;
