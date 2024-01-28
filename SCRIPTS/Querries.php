0adf0b0e886fff5f6bdc3511c6d8bbf0466d8076
Shishwami <157940784+Shishwami@users.noreply.github.com>
2024-01-28 13:42:41 +0800

    Update Querries.php

---
+++ SCRIPTS/Querries.php
@@ -0,0 +1,72 @@
+<?php
+class Querries
+{
+
+    function selectQuerry($tableName, $tableColumns, $tableKeysAndValues)
+    {
+        $sql = "SELECT ";
+        if ($tableColumns == "") {
+            $sql .= "* ";
+        } else {
+            $sql .= "`" . implode("` , `", $tableColumns) . "`";
+        }
+        $sql .= " FROM `" . $tableName . "`";
+        if ($tableKeysAndValues !== "") {
+
+            $sql .= " WHERE ";
+
+            $tableKeys = array_keys($tableKeysAndValues);
+            $tableValues = array_values($tableKeysAndValues);
+
+            for ($i = 0; $i < count($tableKeysAndValues); $i++) {
+                $sql .= "" . $tableKeys[$i] . " = '" . $tableValues[$i] . "' ";
+                if ($i < (count($tableKeys) - 1)) {
+                    $sql .= " AND ";
+                }
+            }
+        }
+        return $sql;
+    }
+
+    function insertQuerry($tableName, $tableKeysAndValues)
+    {
+        $tableKeys = array_keys($tableKeysAndValues);
+        $tableValues = array_values($tableKeysAndValues);
+
+       
+        $sql = "INSERT INTO `" . $tableName . "` (`" . implode('` , `', $tableKeys) . "`) VALUES ('" . implode("' , '", $tableValues) . "')";
+        return $sql;
+    }
+
+    function updateQuerry($tableName, $tableKeysAndValues, $condition)
+    {
+        $tableKeys = array_keys($tableKeysAndValues);
+        $tableValues = array_values($tableKeysAndValues);
+
+
+        $sql = "UPDATE `" . $tableName . "` SET ";
+
+        for ($i = 0; $i < count($tableKeysAndValues); $i++) {
+            $sql .= "`" . $tableKeys[$i] . "` = '" . $tableValues[$i] . "' ";
+            if ($i < (count($tableKeysAndValues) - 1)) {
+                $sql .= ", ";
+            }
+        }
+
+        $conditionKeys = array_keys($condition);
+        $conditionValues = array_values($condition);
+
+        $sql .= " WHERE ";
+
+        for ($i = 0; $i < count($condition); $i++) {
+            $sql .= "`" . $conditionKeys[$i] . "` = '" . $conditionValues[$i] . "'";
+        }
+        echo $sql;
+
+        return $sql;
+    }
+}
+
+
+
+?>
