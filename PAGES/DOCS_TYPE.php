82cbc54329cb8b112e3cfc2427109db22e9ea348
Shishwami <157940784+Shishwami@users.noreply.github.com>
2024-01-28 15:48:03 +0800

    Update DOCS_TYPE.php

--- PAGES/DOCS_TYPE.php
+++ PAGES/DOCS_TYPE.php
@@ -42,21 +42,46 @@
         <input type="button" value="Delete">
         <input type="submit" value="Edit">
     </form>
+    <script src="../SCRIPTS/Constants.js"></script>
+    <script type="module">
 
+        console.log("script red");
+
+        import MyAjax from "../SCRIPTS/MyAjax.js";
+        import JsFunctions from "../SCRIPTS/JsFunctions.js";
+
+        const DOC_TYPE_ADD = document.getElementById("DOC_TYPE_ADD");
+
+        DOC_TYPE_ADD.addEventListener('submit', function (e) {
+            const DOC_TYPE_ADD_NAME = DOC_TYPE_ADD.querySelector('#DOC_TYPE_ADD_NAME');
+            const DOC_TYPE_ADD_CODE = DOC_TYPE_ADD.querySelector('#DOC_TYPE_ADD_CODE');
+
+            JsFunctions.disableFormDefault(e);
+
+            const data = {
+                TABLE_NAME: _TABLE.DOTS_DOC_TYPE.NAME,
+                REQUEST: _REQUEST.INSERT
+            };
+
+            data[_TABLE.DOTS_DOC_TYPE.DOC_TYPE_NAME] = DOC_TYPE_ADD_NAME.value;
+            data[_TABLE.DOTS_DOC_TYPE.DOC_TYPE_CODE] = DOC_TYPE_ADD_CODE.value;
+
+            MyAjax.createJSON((error, response) => {
+                if (error) {
+
+                } else {
+                    if (response.valid) {
+
+                    } else {
+
+                    }
+                }
+            }, data);
+
+            console.log(data);
+        });
+
+    </script>
 </body>
 
 </html>
-
-<script type="module" src="../SCRIPTS/JsFunctions.js">
-
-    import MyAjax from "./MyAjax.js";
-
-    const DOC_TYPE_ADD = document.getElementById("DOC_TYPE_ADD");
-
-    DOC_TYPE_ADD.addEventListener('submit', function (e) {
-        const DOC_TYPE_NAME = DOC_TYPE_ADD.querySelector('#');
-    });
-
-
-
-</script>
