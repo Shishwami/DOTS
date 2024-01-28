6aa7703757dacadc15067d20ef7f471a338cade6
Shishwami <157940784+Shishwami@users.noreply.github.com>
2024-01-28 16:56:00 +0800

    Update DOTS_API.php

--- SCRIPTS/DOTS_API.php
+++ SCRIPTS/DOTS_API.php
@@ -40,22 +40,40 @@ function INSERT($inputs, $conn)
 function SELECT($inputs, $conn)
 {
     $querries = new Querries();
-    $valid = false;
+    $valid = true;
 
     $TABLE_NAME = $inputs['TABLE_NAME'];
     unset($inputs['TABLE_NAME']);
+    $COLUMNS = $inputs['COLUMNS'];
+    unset($inputs['COLUMNS']);
 
-    $sql = $querries->insertQuerry($TABLE_NAME, $inputs);
+    $sql = $querries->selectQuerry($TABLE_NAME, $COLUMNS, $inputs);
 
-    if (mysqli_query($conn, $sql)) {
+
+    $result = mysqli_query($conn, $sql);
+    if ($result) {
         $valid = true;
+        $rows = array();
+        while ($row = mysqli_fetch_assoc($result)) {
+            $rows[] = $row;
+        }
+        mysqli_free_result($result);
+
+        echo json_encode(
+            array(
+                'VALID' => $valid,
+                'RESULT' => $rows
+            )
+        );
+    } else {
+        echo json_encode(
+            array(
+                'VALID' => $valid,
+            )
+        );
     }
 
-    echo json_encode(
-        array(
-            'VALID' => $valid
-        )
-    );
+
 }
 
 
