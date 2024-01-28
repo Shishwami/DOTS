d99a66d1ba734d22babeb80f85727f94f24a9c97
Shishwami <157940784+Shishwami@users.noreply.github.com>
2024-01-28 13:50:47 +0800

    Update DOCS_TYPE.php

--- PAGES/DOCS_TYPE.php
+++ PAGES/DOCS_TYPE.php
@@ -1,15 +1,26 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
     <table>
         <tbody>
-            
+
         </tbody>
     </table>
+
+    <form action="submit" id="DOC_TYPE_ADD">
+        <input type="submit" value="Add">
+    </form>
+
+    <form action="submit" id="DOC_TYPE_EDIT">
+        <input type="submit" value="Add">
+    </form>
 </body>
+
 </html>
