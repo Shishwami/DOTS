5254adc1b70c951cc05e09ae0aebfb42cde59504
Shishwami <157940784+Shishwami@users.noreply.github.com>
2024-01-28 13:44:17 +0800

    Update Constants.js

---
+++ SCRIPTS/Constants.js
@@ -0,0 +1,15 @@
+_TABLE = {
+    DOTS_DOC: {
+
+    },
+    DOTS_DOC_TYPE: {
+        NAME: 'DOTS_DOC_TYPE',
+        ID: 'ID',
+        DOC_TYPE: 'DOC_TYPE',
+        DOC_TYPE_CODE: 'DOC_TYPE_CODE'
+    }
+}
+_REQUEST = {
+    INSERT: 'INSERT',
+    SELECT: 'SELECT',
+}
