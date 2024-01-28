88c53daf3dbbf2c5d8debbd16c7e67a12ded6fa9
Shishwami <157940784+Shishwami@users.noreply.github.com>
2024-01-28 16:56:02 +0800

    Update JsFunctions.js

--- SCRIPTS/JsFunctions.js
+++ SCRIPTS/JsFunctions.js
@@ -8,6 +8,30 @@ class JsFunctions {
     static disableFormButton(element) {
         element.disabled = true;
     }
+    static updateTable(tableJSON, thead, tbody) {
+
+
+        const keys = Object.keys(tableJSON[0]);
+        keys.forEach(key => {
+            const th = document.createElement('th');
+            th.textContent = key;
+            thead.appendChild(th);
+        });
+        tableJSON.forEach(item => {
+            // Create a new row
+            const row = document.createElement('tr');
+
+            // Populate the row with data from each object
+            Object.values(item).forEach(value => {
+                const cell = document.createElement('td');
+                cell.textContent = value;
+                row.appendChild(cell);
+            });
+
+            // Append the row to the table body
+            tbody.appendChild(row);
+        });
+    }
 }
 
 export default JsFunctions;
