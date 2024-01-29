c3b542d6f4187639fffdd66b6e868baf8b9fe23d
Shishwami <157940784+Shishwami@users.noreply.github.com>
2024-01-29 14:31:48 +0800

    Update JsFunctions.js

--- SCRIPTS/JsFunctions.js
+++ SCRIPTS/JsFunctions.js
@@ -17,24 +17,29 @@ class JsFunctions {
         thead.innerHTML = '';
         tbody.innerHTML = '';
 
+        if(tableJSON[0]==null){
+            return
+        }
+        
         const keys = Object.keys(tableJSON[0]);
+
         keys.forEach(key => {
             const th = document.createElement('th');
             th.textContent = _SUB_NAME[key];
             thead.appendChild(th);
         });
         tableJSON.forEach(item => {
-           
+
             const row = document.createElement('tr');
             let found = 0;
-           
+
             Object.entries(item).forEach(([key, value]) => {
                 const cell = document.createElement('td');
                 cell.textContent = value;
-                cell.dataset.keys = key; 
-                cell.dataset.value = value; 
+                cell.dataset.keys = key;
+                cell.dataset.value = value;
                 row.appendChild(cell);
-                
+
                 if (value.toUpperCase().indexOf(filter) > -1) {
                     found++;
                 }
@@ -43,7 +48,7 @@ class JsFunctions {
             if (found == 0) {
                 row.style.display = "none";
             }
-            
+
             tbody.appendChild(row);
         });
     }
