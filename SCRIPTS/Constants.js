da4f89b1aed2546f35c9dedbe8fccdb2d10a451c
Shishwami <157940784+Shishwami@users.noreply.github.com>
2024-01-29 09:21:02 +0800

    Update Constants.js

--- SCRIPTS/Constants.js
+++ SCRIPTS/Constants.js
@@ -7,6 +7,13 @@ _TABLE = {
         ID: 'ID',
         DOC_TYPE_NAME: 'DOC_TYPE_NAME',
         DOC_TYPE_CODE: 'DOC_TYPE_CODE'
+    },
+
+    DOTS_DOC_PRPS: {
+        NAME: 'DOTS_DOC_PRPS',
+        ID: 'ID',
+        DOC_PRPS_NAME: 'DOC_PRPS_NAME',
+        DOC_PRPS_CODE: 'DOC_PRPS_CODE'
     }
 }
 _REQUEST = {
