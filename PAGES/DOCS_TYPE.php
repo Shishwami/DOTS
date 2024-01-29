0b8445d3bcf25c2f7669aa660927f0733971a780
Shishwami <157940784+Shishwami@users.noreply.github.com>
2024-01-29 09:29:42 +0800

    Update DOCS_TYPE.php

--- PAGES/DOCS_TYPE.php
+++ PAGES/DOCS_TYPE.php
@@ -57,7 +57,7 @@
         const DOC_TYPE_TBL = document.getElementById("TABLE_DOC_TYPE");
 
         updateTable();
-        setInterval(updateTable, 2000);
+        setInterval(updateTable, _RESET_TIME);
 
         DOC_TYPE_ADD.addEventListener('submit', function (e) {
             const DOC_TYPE_ADD_NAME = DOC_TYPE_ADD.querySelector('#DOC_TYPE_ADD_NAME');
