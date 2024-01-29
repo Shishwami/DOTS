a554913e0cdba0ef0007639724c1e977dfb861aa
Shishwami <157940784+Shishwami@users.noreply.github.com>
2024-01-29 13:32:46 +0800

    Update DOCS_PRPS.php

--- PAGES/DOCS_PRPS.php
+++ PAGES/DOCS_PRPS.php
@@ -1,214 +1,239 @@
 <!DOCTYPE html>
-    <html lang="en">
+<html lang="en">
 
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Document</title>
-    </head>
-    <style>
-        tr:hover{
-            background-color: green;
-        }
-    </style>
-    <body>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<style>
+    tr:hover {
+        background-color: green;
+    }
+</style>
+
+<body>
+    <div>
+        <label for="searchBar">Search:</label>
+        <input type="text" name="searchBar" id="searchBar" placeholder="Search">
+    </div>
+    <table id="TABLE_DOC_PRPS">
+        <thead>
+            <tr>
+
+            </tr>
+        </thead>
+        <tbody>
+
+        </tbody>
+    </table>
+
+    <form action="submit" id="FORM_DOC_PRPS_ADD">
         <div>
-            <label for="searchBar">Search:</label>
-            <input type="text" name="searchBar" id="searchBar" placeholder="Search">
+            <label for="DOC_PRPS_ADD_NAME">Purpose Type:</label>
+            <br>
+            <input type="text" name="DOC_PRPS_ADD_NAME" id="DOC_PRPS_ADD_NAME">
         </div>
-        <table id="TABLE_DOC_PRPS">
-            <thead>
-                <tr>
+        <div>
+            <label for="DOC_PRPS_ADD_CODE">Purpose Code:</label>
+            <br>
+            <input type="text" name="DOC_PRPS_ADD_CODE" id="DOC_PRPS_ADD_CODE">
+        </div>
+        <input type="submit" value="Add">
+    </form>
 
-                </tr>
-            </thead>
-            <tbody>
+    <form action="submit" id="FORM_DOC_PRPS_EDIT">
+        <div>
+            <label for="DOC_PRPS_EDIT_NAME">Purpose Type:</label>
+            <br>
+            <input type="text" name="DOC_PRPS_EDIT_NAME" id="DOC_PRPS_EDIT_NAME" data-keys="DOC_PRPS_NAME">
+        </div>
+        <div>
+            <label for="DOC_PRPS_EDIT_CODE">Purpose Code:</label>
+            <br>
+            <input type="text" name="DOC_PRPS_EDIT_CODE" id="DOC_PRPS_EDIT_CODE" data-keys="DOC_PRPS_CODE">
+        </div>
+        <input type="button" value="Delete">
+        <input type="submit" value="Edit">
+    </form>
+    <script src="../SCRIPTS/Constants.js"></script>
+    <script type="module">
 
-            </tbody>
-        </table>
+        import MyAjax from "../SCRIPTS/MyAjax.js";
+        import JsFunctions from "../SCRIPTS/JsFunctions.js";
 
-        <form action="submit" id="FORM_DOC_PRPS_ADD">
-            <div>
-                <label for="DOC_PRPS_ADD_NAME">Document Type:</label>
-                <br>
-                <input type="text" name="DOC_PRPS_ADD_NAME" id="DOC_PRPS_ADD_NAME">
-            </div>
-            <div>
-                <label for="DOC_PRPS_ADD_CODE">Document Code:</label>
-                <br>
-                <input type="text" name="DOC_PRPS_ADD_CODE" id="DOC_PRPS_ADD_CODE">
-            </div>
-            <input type="submit" value="Add">
-        </form>
+        const DOC_PRPS_ADD = document.getElementById("FORM_DOC_PRPS_ADD");
+        const DOC_PRPS_EDIT = document.getElementById("FORM_DOC_PRPS_EDIT");
+        const DOC_PRPS_DELETE_BTTN = DOC_PRPS_EDIT.querySelector("input[type=button]");
+        const DOC_PRPS_TBL = document.getElementById("TABLE_DOC_PRPS");
 
-        <form action="submit" id="FORM_DOC_PRPS_EDIT">
-            <div>
-                <label for="DOC_PRPS_EDIT_NAME">Document Type:</label>
-                <br>
-                <input type="text" name="DOC_PRPS_EDIT_NAME" id="DOC_PRPS_EDIT_NAME" data-keys="DOC_PRPS_NAME">
-            </div>
-            <div>
-                <label for="DOC_PRPS_EDIT_CODE">Document Code:</label>
-                <br>
-                <input type="text" name="DOC_PRPS_EDIT_CODE" id="DOC_PRPS_EDIT_CODE" data-keys="DOC_PRPS_CODE">
-            </div>
-            <input type="button" value="Delete">
-            <input type="submit" value="Edit">
-        </form>
-        <script src="../SCRIPTS/Constants.js"></script>
-        <script type="module">
+        console.log(DOC_PRPS_DELETE_BTTN);
 
-            import MyAjax from "../SCRIPTS/MyAjax.js";
-            import JsFunctions from "../SCRIPTS/JsFunctions.js";
+        updateTable();
+        setInterval(updateTable, _RESET_TIME);
 
-            const DOC_PRPS_ADD = document.getElementById("FORM_DOC_PRPS_ADD");
-            const DOC_PRPS_EDIT = document.getElementById("FORM_DOC_PRPS_EDIT");
-            const DOC_PRPS_DELETE =DOC_PRPS_EDIT.querySelector("input[type=button]");
-            const DOC_PRPS_TBL = document.getElementById("TABLE_DOC_PRPS");
+        DOC_PRPS_ADD.addEventListener('submit', function (e) {
 
-            console.log(DOC_PRPS_DELETE);
+            const DOC_PRPS_ADD_NAME = DOC_PRPS_ADD.querySelector('#DOC_PRPS_ADD_NAME');
+            const DOC_PRPS_ADD_CODE = DOC_PRPS_ADD.querySelector('#DOC_PRPS_ADD_CODE');
+            const DOC_PRPS_ADD_BTTN = DOC_PRPS_ADD.querySelector("input[type=submit]");
 
-            updateTable();
-            setInterval(updateTable, _RESET_TIME);
+            JsFunctions.disableFormDefault(e);
+            JsFunctions.disableFormButton(DOC_PRPS_ADD_BTTN);
 
-            DOC_PRPS_ADD.addEventListener('submit', function (e) {
+            const data = {
+                TABLE_NAME: _TABLE.DOTS_DOC_PRPS.NAME,
+                REQUEST: _REQUEST.INSERT
+            };
 
-                const DOC_PRPS_ADD_NAME = DOC_PRPS_ADD.querySelector('#DOC_PRPS_ADD_NAME');
-                const DOC_PRPS_ADD_CODE = DOC_PRPS_ADD.querySelector('#DOC_PRPS_ADD_CODE');
-                const DOC_PRPS_ADD_BTTN = DOC_PRPS_ADD.querySelector("input[type=submit]");
+            data[_TABLE.DOTS_DOC_PRPS.DOC_PRPS_NAME] = DOC_PRPS_ADD_NAME.value;
+            data[_TABLE.DOTS_DOC_PRPS.DOC_PRPS_CODE] = DOC_PRPS_ADD_CODE.value;
 
-                JsFunctions.disableFormDefault(e);
-                JsFunctions.disableFormButton(DOC_PRPS_ADD_BTTN);
-
-                const data = {
-                    TABLE_NAME: _TABLE.DOTS_DOC_PRPS.NAME,
-                    REQUEST: _REQUEST.INSERT
-                };
-
-                data[_TABLE.DOTS_DOC_PRPS.DOC_PRPS_NAME] = DOC_PRPS_ADD_NAME.value;
-                data[_TABLE.DOTS_DOC_PRPS.DOC_PRPS_CODE] = DOC_PRPS_ADD_CODE.value;
-
-                MyAjax.createJSON((error, response) => {
-                    if (error) {
-                        console.log(error);
+            MyAjax.createJSON((error, response) => {
+                if (error) {
+                    console.log(error);
+                } else {
+                    if (response.VALID) {
+                        //console.log(response);
+                        //success message
+                        JsFunctions.clearInputText(DOC_PRPS_ADD_NAME);
+                        JsFunctions.clearInputText(DOC_PRPS_ADD_CODE);
+                        JsFunctions.enableFormButton(DOC_PRPS_ADD_BTTN);
                     } else {
-                        if (response.VALID) {
-                            //console.log(response);
-                            //success message
-                            JsFunctions.clearInputText(DOC_PRPS_ADD_NAME);
-                            JsFunctions.clearInputText(DOC_PRPS_ADD_CODE);
-                            JsFunctions.enableFormButton(DOC_PRPS_ADD_BTTN);
-                        } else {
-                            // console.log(response);
-                            //error message
-                        }
+                        // console.log(response);
+                        //error message
                     }
-                }, data);
-
-                console.log(data);
-            });
-
-            DOC_PRPS_EDIT.addEventListener('submit', function (e) {
-
-                const DOC_PRPS_EDIT_NAME = DOC_PRPS_EDIT.querySelector('#DOC_PRPS_EDIT_NAME');
-                const DOC_PRPS_EDIT_CODE = DOC_PRPS_EDIT.querySelector('#DOC_PRPS_EDIT_CODE');
-                const DOC_PRPS_EDIT_BTTN = DOC_PRPS_EDIT.querySelector("input[type=submit]");
-
-                JsFunctions.disableFormDefault(e);
-                JsFunctions.disableFormButton(DOC_PRPS_EDIT_BTTN);
-
-                const data = {
-                    TABLE_NAME: _TABLE.DOTS_DOC_PRPS.NAME,
-                    REQUEST: _REQUEST.UPDATE,
-                    CONDITION: DOC_PRPS_EDIT_BTTN.dataset.condition
-                };
-
-                data[_TABLE.DOTS_DOC_PRPS.DOC_PRPS_NAME] = DOC_PRPS_EDIT_NAME.value;
-                data[_TABLE.DOTS_DOC_PRPS.DOC_PRPS_CODE] = DOC_PRPS_EDIT_CODE.value;
-
-                console.log(data);
-
-                MyAjax.createJSON((error, response) => {
-                    if (error) {
-                        console.log(error);
-                    } else {
-                        if (response.VALID) {
-                            //console.log(response);
-                            //success message
-                            JsFunctions.clearInputText(DOC_PRPS_EDIT_NAME);
-                            JsFunctions.clearInputText(DOC_PRPS_EDIT_CODE);
-                            JsFunctions.enableFormButton(DOC_PRPS_EDIT_BTTN);
-                        } else {
-                            // console.log(response);
-                            //error message
-                        }
-                    }
-                }, data);
-
-                console.log(data);
-            });
-
-            function updateTable() {
-                const TBL_BODY = DOC_PRPS_TBL.querySelector("tbody");
-                const TBL_HEAD = DOC_PRPS_TBL.querySelector("thead");
-
-                const data = {
-                    TABLE_NAME: _TABLE.DOTS_DOC_PRPS.NAME,
-                    REQUEST: _REQUEST.SELECT,
-                    COLUMNS: ""
-                };
-
-                MyAjax.createJSON((error, response) => {
-                    if (error) {
-                        console.log(error);
-                    } else {
-                        if (response.VALID) {
-                            // console.log(response.RESULT);
-                            JsFunctions.updateTable(response.RESULT, TBL_HEAD, TBL_BODY);
-                            addEventOnTR(TBL_HEAD, TBL_BODY);
-                        } else {
-                            //  console.log(response);
-                        }
-                    }
-                }, data);
-
-            }
-
-            function addEventOnTR(thead, tbody) {
-                const TrList = tbody.getElementsByTagName('tr');
-                const DOC_PRPS_EDIT_NAME = DOC_PRPS_EDIT.querySelector('#DOC_PRPS_EDIT_NAME');
-                const DOC_PRPS_EDIT_CODE = DOC_PRPS_EDIT.querySelector('#DOC_PRPS_EDIT_CODE');
-                const DOC_PRPS_EDIT_BTTN = DOC_PRPS_EDIT.querySelector("input[type=submit]");
-
-                for (let index = 0; index < TrList.length; index++) {
-                    TrList[index].addEventListener('click', function (e) {
-
-                        const Tr = this;
-                        const Td = this.getElementsByTagName('td');
-                        const TdValues = [];
-                        const TdKeys = [];
-                        const TdKeysAndValues = {};
-
-                        for (let i = 0; i < Td.length; i++) {
-                            TdValues.push(Td[i].dataset.value);
-                            TdKeys.push(Td[i].dataset.keys);
-                            TdKeysAndValues[TdKeys[i]] = TdValues[i];
-
-                            if (DOC_PRPS_EDIT_NAME.dataset.keys == TdKeys[i]) {
-                                DOC_PRPS_EDIT_NAME.value = TdValues[i];
-                            }
-                            if (DOC_PRPS_EDIT_CODE.dataset.keys == TdKeys[i]) {
-                                DOC_PRPS_EDIT_CODE.value = TdValues[i];
-                            }
-                        }
-
-                        DOC_PRPS_EDIT_BTTN.dataset.condition = JSON.stringify(TdKeysAndValues);
-                    });
                 }
+            }, data);
+
+            console.log(data);
+        });
+
+        DOC_PRPS_EDIT.addEventListener('submit', function (e) {
+
+            const DOC_PRPS_EDIT_NAME = DOC_PRPS_EDIT.querySelector('#DOC_PRPS_EDIT_NAME');
+            const DOC_PRPS_EDIT_CODE = DOC_PRPS_EDIT.querySelector('#DOC_PRPS_EDIT_CODE');
+            const DOC_PRPS_EDIT_BTTN = DOC_PRPS_EDIT.querySelector("input[type=submit]");
+
+            JsFunctions.disableFormDefault(e);
+            JsFunctions.disableFormButton(DOC_PRPS_EDIT_BTTN);
+
+            const data = {
+                TABLE_NAME: _TABLE.DOTS_DOC_PRPS.NAME,
+                REQUEST: _REQUEST.UPDATE,
+                CONDITION: DOC_PRPS_EDIT_BTTN.dataset.condition
+            };
+
+            data[_TABLE.DOTS_DOC_PRPS.DOC_PRPS_NAME] = DOC_PRPS_EDIT_NAME.value;
+            data[_TABLE.DOTS_DOC_PRPS.DOC_PRPS_CODE] = DOC_PRPS_EDIT_CODE.value;
+
+            MyAjax.createJSON((error, response) => {
+                if (error) {
+                    console.log(error);
+                } else {
+                    if (response.VALID) {
+                        //console.log(response);
+                        //success message
+                        JsFunctions.clearInputText(DOC_PRPS_EDIT_NAME);
+                        JsFunctions.clearInputText(DOC_PRPS_EDIT_CODE);
+                        JsFunctions.enableFormButton(DOC_PRPS_EDIT_BTTN);
+                    } else {
+                        // console.log(response);
+                        //error message
+                    }
+                }
+            }, data);
+
+            console.log(data);
+        });
+
+        DOC_PRPS_DELETE_BTTN.addEventListener('click', function (e) {
+            const data = {
+                TABLE_NAME: _TABLE.DOTS_DOC_PRPS.NAME,
+                REQUEST: _REQUEST.DELETE,
+                CONDITION: DOC_PRPS_DELETE_BTTN.dataset.condition
+            };  
+
+            console.log(data);
+
+            MyAjax.createJSON((error, response) => {
+                if (error) {
+                    console.log(error);
+                } else {
+                    if (response.VALID) {
+                        console.log(response);
+                        //success message
+                    } else {
+                        // console.log(response);
+                        //error message
+                    }
+                }
+            }, data);
+
+        });
+
+        function updateTable() {
+            const TBL_BODY = DOC_PRPS_TBL.querySelector("tbody");
+            const TBL_HEAD = DOC_PRPS_TBL.querySelector("thead");
+
+            const data = {
+                TABLE_NAME: _TABLE.DOTS_DOC_PRPS.NAME,
+                REQUEST: _REQUEST.SELECT,
+                COLUMNS: ""
+            };
+
+            MyAjax.createJSON((error, response) => {
+                if (error) {
+                    console.log(error);
+                } else {
+                    if (response.VALID) {
+                        // console.log(response.RESULT);
+                        JsFunctions.updateTable(response.RESULT, TBL_HEAD, TBL_BODY);
+                        addEventOnTR(TBL_HEAD, TBL_BODY);
+                    } else {
+                        //  console.log(response);
+                    }
+                }
+            }, data);
+
+        }
+
+        function addEventOnTR(thead, tbody) {
+            const TrList = tbody.getElementsByTagName('tr');
+            const DOC_PRPS_EDIT_NAME = DOC_PRPS_EDIT.querySelector('#DOC_PRPS_EDIT_NAME');
+            const DOC_PRPS_EDIT_CODE = DOC_PRPS_EDIT.querySelector('#DOC_PRPS_EDIT_CODE');
+            const DOC_PRPS_EDIT_BTTN = DOC_PRPS_EDIT.querySelector("input[type=submit]");
+
+            for (let index = 0; index < TrList.length; index++) {
+                TrList[index].addEventListener('click', function (e) {
+
+                    const Tr = this;
+                    const Td = this.getElementsByTagName('td');
+                    const TdValues = [];
+                    const TdKeys = [];
+                    const TdKeysAndValues = {};
+
+                    for (let i = 0; i < Td.length; i++) {
+                        TdValues.push(Td[i].dataset.value);
+                        TdKeys.push(Td[i].dataset.keys);
+                        TdKeysAndValues[TdKeys[i]] = TdValues[i];
+
+                        if (DOC_PRPS_EDIT_NAME.dataset.keys == TdKeys[i]) {
+                            DOC_PRPS_EDIT_NAME.value = TdValues[i];
+                        }
+                        if (DOC_PRPS_EDIT_CODE.dataset.keys == TdKeys[i]) {
+                            DOC_PRPS_EDIT_CODE.value = TdValues[i];
+                        }
+                    }
+
+                    DOC_PRPS_EDIT_BTTN.dataset.condition = JSON.stringify(TdKeysAndValues);
+                    DOC_PRPS_DELETE_BTTN.dataset.condition = JSON.stringify(TdKeysAndValues);
+                });
             }
+        }
 
 
-        </script>
-    </body>
+    </script>
+</body>
 
-    </html>
+</html>
