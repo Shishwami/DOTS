5fdec8297d55a555445e8fd8beb951da6a96fd58
Shishwami <157940784+Shishwami@users.noreply.github.com>
2024-01-29 09:29:37 +0800

    Update DOCS_PRPS.php

--- PAGES/DOCS_PRPS.php
+++ PAGES/DOCS_PRPS.php
@@ -57,13 +57,13 @@
         const DOC_PRPS_TBL = document.getElementById("TABLE_DOC_PRPS");
 
         updateTable();
-        setInterval(updateTable, 2000);
+        setInterval(updateTable, _RESET_TIME);
 
         DOC_PRPS_ADD.addEventListener('submit', function (e) {
             const DOC_PRPS_ADD_NAME = DOC_PRPS_ADD.querySelector('#DOC_PRPS_ADD_NAME');
             const DOC_PRPS_ADD_CODE = DOC_PRPS_ADD.querySelector('#DOC_PRPS_ADD_CODE');
             const DOC_PRPS_ADD_BTTN = DOC_PRPS_ADD.querySelector("input[type=submit]");
-            
+
             JsFunctions.disableFormDefault(e);
             JsFunctions.disableFormButton(DOC_PRPS_ADD_BTTN);
 
