08978a6d5f93e0a483790acfe45f4e53754ca781
Shishwami <157940784+Shishwami@users.noreply.github.com>
2024-01-30 19:05:01 +0800

    Update DOTS_LOGIN.php

--- PAGES/DOTS_LOGIN.php
+++ PAGES/DOTS_LOGIN.php
@@ -3,11 +3,40 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="DOTS_LOGIN.css">
+    <link rel="stylesheet" href="../DOTS_NAVBAR/DOTS_NAV.css">
+    <title>CHRMO - DOTS</title>
 </head>
 <body>
-    <form action="submit">
-        
-    </form>
+    <?php //include '../DOTS_NAVBAR/DOTS_NAV.php';?>
+    
+    <div class="login-box"> <!-- Login Form Container -->
+
+        <h1>CHRMO - DOTS</h1>
+
+        <form class="login-form" action="DOTS_LOGIN.html" class="login" method="post">
+            <div class="container">
+                <label for="uname"></label>
+                <input type="text" placeholder="Username" name="uname" id="uname" required>
+            </div>
+
+            <div class="container">
+                <label for="pword"></label>
+                <input type="password" placeholder="Password" name="pword" required>
+            </div>
+
+            <div class="container">
+                <input type="checkbox" value="RememberMe" id="RememberMe">
+                <label class="rmb" for="RememberMe">Remember Me</label>
+            </div>
+            <br>
+            <input type="submit" value="Login" name="submit" onclick="RememberMe()">
+        </form>
+
+        <div class="container">
+            <a href="#">Reset Password</a>
+        </div>
+    </div>
+
 </body>
 </html>
