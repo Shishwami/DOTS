e467de1182972fe361b6239d80d675e6ac333082
Shishwami <157940784+Shishwami@users.noreply.github.com>
2024-01-30 19:06:28 +0800

    Update DOTS_LOGIN.php

--- PAGES/DOTS_LOGIN.php
+++ PAGES/DOTS_LOGIN.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="DOTS_LOGIN.css">
+    <link rel="stylesheet" href="../CSS/DOTS_LOGIN.css">
     <link rel="stylesheet" href="../DOTS_NAVBAR/DOTS_NAV.css">
     <title>CHRMO - DOTS</title>
 </head>
