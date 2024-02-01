fa185e7d6cf96103e726d0dbd6bd7f7f97c30621
Shishwami <157940784+Shishwami@users.noreply.github.com>
2024-02-01 14:23:31 +0800

    Update DOCS_VIEW.php

--- PAGES/DOCS_VIEW.php
+++ PAGES/DOCS_VIEW.php
@@ -8,14 +8,56 @@
 </head>
 
 <body>
-    <table id="TABLE_DOC_VIEW">
+    <div>
+        <label for="searchBar">Search:</label>
+        <input type="text" name="searchBar" id="searchBar" placeholder="Search">
+    </div>
+    <table id="DOC_VIEW_TBL">
         <thead>
             <tr></tr>
         </thead>
         <tbody>
-            
+
         </tbody>
     </table>
 </body>
+<script src="../SCRIPTS/Constants.js"></script>
+<script type="module">
+
+    import MyAjax from "../SCRIPTS/MyAjax.js";
+    import JsFunctions from "../SCRIPTS/JsFunctions.js";
+
+    const DOC_VIEW_TBL = document.getElementById("DOC_VIEW_TBL");
+
+    getTable("");
+    setInterval(function () {
+        getTable("");
+    }, _RESET_TIME);
+
+
+    function getTable(filter) {
+        filter = "";
+        const tHead = DOC_VIEW_TBL.querySelector('thead');
+        const tBody = DOC_VIEW_TBL.querySelector('tbody');
+
+        const data = {
+            TABLE_NAME: _TABLE.DOTS_DOC.NAME,
+            REQUEST: _REQUEST.SELECT,
+        }
+        MyAjax.createJSON((error, response) => {
+            if (!error) {
+                if (response.VALID) {
+                    const results = response.RESULT;
+                    JsFunctions.updateTable(results, tHead, tBody, filter);
+                } else {
+                    //no data taken
+                }
+            } else {
+                alert(error);
+            }
+        }, data);
+    }
+
+</script>
 
 </html>
