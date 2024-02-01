9f883ef87c791bf658e74b6bfc38c1b4addbe595
Shishwami <157940784+Shishwami@users.noreply.github.com>
2024-02-01 11:11:10 +0800

    Update Constants.js

--- SCRIPTS/Constants.js
+++ SCRIPTS/Constants.js
@@ -39,6 +39,7 @@ const _TABLE = {
         DOC_PRPS_CODE: 'DOC_PRPS_CODE'
     },
     DOTS_DOC_STATUS: {
+        
         NAME: 'DOTS_DOC_STATUS',
         ID: 'ID',
         DOC_STATUS_NAME: 'DOC_STATUS_NAME',
