c98df41252fbc209c6cea89899dc85ed778f5318
Shishwami <157940784+Shishwami@users.noreply.github.com>
2024-02-05 09:36:26 +0800

    Create DOTS_LOGIN.php

---
+++ PAGES/DOTS_LOGIN/DOTS_LOGIN.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <form action="submit" id="FORM_LOGIN">
+        <div>
+            <label for="USERNAME">Username:</label>
+            <input type="text" name="USERNAME" id="USERNAME">
+        </div>
+        <div>
+            <label for="PASSWORD">Password:</label>
+            <input type="text" name="PASSWORD" id="PASSWORD">
+        </div>
+        <div>
+            <input type="submit" value="LOGIN">
+        </div>
+    </form>
+    <script src="../../SCRIPTS/Constants.js"></script>
+</body>
+
+<script type="module">
+
+    import MyAjax from "../../SCRIPTS/MyAjax.js"
+    import JsFunctions from "../../SCRIPTS/JsFunctions.js"
+
+    const FORM_LOGIN = document.getElementById("FORM_LOGIN");
+
+    FORM_LOGIN.addEventListener('submit', function (e) {
+        e.preventDefault();
+
+        var data = JsFunctions.FormToJson(FORM_LOGIN);
+
+        var columns = [
+            DOTS_ACCOUNT_INFO.HRIS_ID,
+            DOTS_ACCOUNT_INFO.FULL_NAME,
+            DOTS_ACCOUNT_INFO.INITIAL,
+        ]
+
+        data = {
+            ...data,
+            TABLE_NAME: DOTS_ACCOUNT_INFO.NAME,
+            REQUEST: _REQUEST.SELECT,
+            COLUMNS: columns
+        };
+
+
+
+        MyAjax.createJSON((error, response) => {
+            if (!error) {
+                if (response.VALID) {
+                    delete response.VALID;
+                    createSession(response);
+                }
+            } else {
+                alert(error);
+            }
+        }, data);
+    });
+
+    function createSession(response) {
+        const object = Object.values(response)[0];
+
+        var data = {
+            REQUEST: _REQUEST.CREATE_SESSION,
+        }
+        Object.assign(data,object[0]);
+
+        MyAjax.createJSON((error, response) => {
+            if (!error) {
+                if (response.VALID) {
+                    location.href = "../DOC_RECEIVE/DOC_RECEIVE.php";
+                }
+            } else {
+                alert(error);
+            }
+        }, data);
+
+    }
+
+</script>
+
+</html>
