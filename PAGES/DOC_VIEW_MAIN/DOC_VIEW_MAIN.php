b02c3d5e6f44556a730fa6df4b917a026f205c65
Shishwami <157940784+Shishwami@users.noreply.github.com>
2024-02-06 09:05:36 +0800

    Update DOC_VIEW_MAIN.php

--- PAGES/DOC_VIEW_MAIN/DOC_VIEW_MAIN.php
+++ PAGES/DOC_VIEW_MAIN/DOC_VIEW_MAIN.php
@@ -21,9 +21,40 @@
     <div>
         <button name="DOC_SEND" id="DOC_SEND">SEND</button>
         <button name="DOC_EDIT" id="DOC_EDIT">EDIT</button>
+        <button name="DOC_TRACK" id="DOC_TRACK">TRACK</button>
         <button name="DOC_ATTACHMENTS" id="DOC_ATTACHMENTS">ATTACHMENTS</button>
         <button name="DOC_DELETE" id="DOC_DELETE">DELETE</button>
     </div>
+
+    <form action="submit" name="FORM_DOC_SEND">
+        <div>
+            <label for="DOC_NUMBER">Document Number:</label>
+            <input type="text" name="DOC_NUMBER" id="DOC_NUMBER" disabled>
+        </div>
+        <div>
+            <label for="DOC_PRPS">Documnet Purpose:</label>
+            <select name="DOC_PRPS" id="DOC_PRPS">
+
+            </select>
+        </div>
+        <div>
+            <label for="DOC_DIVISION">Division:</label>
+            <select name="DOC_DIVISION" id="DOC_DIVISION">
+                <!-- to be filled using database -->
+            </select>
+        </div>
+        <div>
+            <label for="DOC_ADDRESSEE">Addressee:</label>
+            <select name="DOC_ADDRESSEE" id="DOC_ADDRESSEE">
+                <!-- to be filled using database -->
+            </select>
+        </div>
+        <div>
+            <label for="DOC_NOTES">Notes:</label>
+            <input type="text" name="DOC_NOTES" id="DOC_NOTES">
+        </div>
+        <input type="submit" value="Send">
+    </form>
 </body>
 
 <script src="../../SCRIPTS/Constants.js"></script>
