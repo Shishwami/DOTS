3ca40579c029d8600c5570e7f65a4eafc04a34a4
Shishwami <157940784+Shishwami@users.noreply.github.com>
2024-02-12 15:25:18 +0800

    Update JsFunctions.js

--- SCRIPTS/JsFunctions.js
+++ SCRIPTS/JsFunctions.js
@@ -15,12 +15,7 @@ class JsFunctions {
         const keys = Object.keys(tableJSON[0]);
         keys.forEach(key => {
             const th = document.createElement('th');
-            if (_SUB_NAME[key] == null) {
-                th.textContent = key;
-            } else {
-                th.textContent = _SUB_NAME[key];
-            }
-
+            th.textContent = key;
             thead.appendChild(th);
         });
 
@@ -99,15 +94,15 @@ class JsFunctions {
         return empty;
     }
 
-    static formatDate(date){
+    static formatDate(date) {
         var hours = date.getHours();
         var minutes = date.getMinutes();
         var ampm = hours >= 12 ? 'pm' : 'am';
         hours = hours % 12;
         hours = hours ? hours : 12; // the hour '0' should be '12'
-        minutes = minutes < 10 ? '0'+minutes : minutes;
+        minutes = minutes < 10 ? '0' + minutes : minutes;
         var strTime = hours + ':' + minutes + ' ' + ampm;
-        return (date.getMonth()+1) + "/" + date.getDate() + "/" + date.getFullYear() + "  " + strTime;
+        return (date.getMonth() + 1) + "/" + date.getDate() + "/" + date.getFullYear() + "  " + strTime;
     }
 
     static tbodyEventListener(tbody) {
