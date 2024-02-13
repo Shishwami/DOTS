2ea6315bec3e26d649656bbda4797c436892bf50
Shishwami <157940784+Shishwami@users.noreply.github.com>
2024-02-13 10:46:24 +0800

    Update DOC_VIEW.php

--- PAGES/DOC_VIEW/DOC_VIEW.php
+++ PAGES/DOC_VIEW/DOC_VIEW.php
@@ -136,8 +136,8 @@
                 <input type="text" name="DOC_NOTES" id="SEND_DOC_NOTES">
             </div>
 
-            <input type="text" name="ACTION_ID" id="SEND_DOC_ACTION" value="3">
-            <input type="text" name="S_USER_ID" id="SEND_S_USER_ID" value="">
+            <input type="text" name="ACTION_ID" id="SEND_DOC_ACTION" value="3" hidden>
+            <input type="text" name="S_USER_ID" id="SEND_S_USER_ID" value="" hidden>
 
             <input type="submit" value="Send">
         </form>
