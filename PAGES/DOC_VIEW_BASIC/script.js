a7df7c80eec838e4ec73522bb1a4c447e063685a
Shishwami <157940784+Shishwami@users.noreply.github.com>
2024-02-13 14:51:44 +0800

    Update script.js

--- PAGES/DOC_VIEW_BASIC/script.js
+++ PAGES/DOC_VIEW_BASIC/script.js
@@ -8,6 +8,9 @@ const DOC_VIEW_BASIC = document.getElementById("DOC_VIEW_BASIC");
 setSession();
 
 setTable("");
+searchBar.addEventListener('input', function () {
+    setTable(searchBar.value.toUpperCase());
+});
 setInterval(function () {
     setTable(searchBar.value.toUpperCase());
 }, _RESET_TIME);
@@ -15,9 +18,91 @@ setInterval(function () {
 
 
 function setTable(filter) {
+
+
+
+    const columns = [
+        'DOC_NUM',
+        DOTS_DOCUMENT_SUB.NAME + "." + DOTS_DOCUMENT_SUB.ID,
+        'DOC_NOTES',
+        DOTS_DOC_PRPS.DOC_PRPS,
+        // 'S_OFFICE.DOC_OFFICE AS Office1',
+        // 'S_DEPT.DOC_DEPT AS Department',
+        // 'S_FULL_NAME.FULL_NAME as sname',
+
+        "CONCAT(" +
+        "IF(S_OFFICE.DOC_OFFICE IS NOT NULL,CONCAT(S_OFFICE.DOC_OFFICE,'-'), ' '),' ', " +
+        "IF(S_DEPT.DOC_DEPT IS NOT NULL,CONCAT(S_DEPT.DOC_DEPT,'-'), ' '), " +
+        "IFNULL(S_FULL_NAME.FULL_NAME, ' ')) as 'Sender'",
+
+        // 'R_OFFICE.DOC_OFFICE AS Office2',
+        // 'R_DEPT.DOC_DEPT AS Department2',
+        // 'R_FULL_NAME.FULL_NAME',
+
+        "CONCAT(" +
+        "IF(R_OFFICE.DOC_OFFICE IS NOT NULL,CONCAT(R_OFFICE.DOC_OFFICE,'-'), ' '),' ', " +
+        "IF(R_DEPT.DOC_DEPT IS NOT NULL,CONCAT(R_DEPT.DOC_DEPT,'-'), ' '), " +
+        "IFNULL(R_FULL_NAME.FULL_NAME, ' ')) as 'Receiver'",
+
+        'DATE_TIME_RECEIVED',
+        DOTS_DOC_ACTION.DOC_ACTION
+    ];
     var data = {
         TABLE: DOTS_DOCUMENT_SUB.NAME,
         REQUEST: _REQUEST.SELECT,
+        COLUMNS: columns,
+        JOIN: [
+            {
+                table: DOTS_DOC_PRPS.NAME,
+                ON: [DOTS_DOCUMENT_SUB.NAME + "." + DOTS_DOCUMENT_SUB.PRPS_ID
+                    + " = " + DOTS_DOC_PRPS.NAME + "." + DOTS_DOC_PRPS.ID],
+                TYPE: 'LEFT'
+            },
+            {
+                table: DOTS_DOC_OFFICE.NAME + " S_OFFICE",
+                ON: [DOTS_DOCUMENT_SUB.NAME + "." + DOTS_DOCUMENT_SUB.S_OFFICE_ID +
+                    " = " + "S_OFFICE." + DOTS_DOC_OFFICE.ID],
+                TYPE: 'LEFT'
+            },
+            {
+                table: DOTS_DOC_DEPT.NAME + " S_DEPT",
+                ON: [DOTS_DOCUMENT_SUB.NAME + "." + DOTS_DOCUMENT_SUB.S_DEPT_ID
+                    + " = " + "S_DEPT." + DOTS_DOC_DEPT.ID],
+                TYPE: 'LEFT'
+            },
+            {
+                table: DOTS_ACCOUNT_INFO.NAME + " S_FULL_NAME",
+                ON: [DOTS_DOCUMENT_SUB.NAME + "." + DOTS_DOCUMENT_SUB.S_USER_ID
+                    + " = " + "S_FULL_NAME." + DOTS_ACCOUNT_INFO.HRIS_ID],
+                TYPE: 'LEFT',
+            },
+            {
+                table: DOTS_DOC_OFFICE.NAME + " R_OFFICE",
+                ON: [DOTS_DOCUMENT_SUB.NAME + "." + DOTS_DOCUMENT_SUB.R_OFFICE_ID
+                    + " = " + "R_OFFICE." + DOTS_DOC_OFFICE.ID],
+                TYPE: 'LEFT'
+            },
+            {
+                table: DOTS_DOC_DEPT.NAME + " R_DEPT",
+                ON: [DOTS_DOCUMENT_SUB.NAME + "." + DOTS_DOCUMENT_SUB.R_DEPT_ID
+                    + " = " + "R_DEPT." + DOTS_DOC_DEPT.ID],
+                TYPE: 'LEFT'
+            },
+            {
+                table: DOTS_ACCOUNT_INFO.NAME + " R_FULL_NAME",
+                ON: [DOTS_DOCUMENT_SUB.NAME + "." + DOTS_DOCUMENT_SUB.R_USER_ID
+                    + " = " + "R_FULL_NAME." + DOTS_ACCOUNT_INFO.HRIS_ID],
+                TYPE: 'LEFT',
+            },
+            {
+                table: DOTS_DOC_ACTION.NAME,
+                ON: [DOTS_DOCUMENT_SUB.NAME + "." + DOTS_DOCUMENT_SUB.ACTION_ID
+                    + " = " + DOTS_DOC_ACTION.NAME + "." + DOTS_DOC_ACTION.ID],
+                TYPE: 'LEFT',
+            },
+
+        ],
+        ORDER_BY: DOTS_DOCUMENT_SUB.NAME + "." + DOTS_DOCUMENT_SUB.ID + ' DESC',
         WHERE: {
             OR: {}
         }
