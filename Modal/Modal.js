bfebd815fc7950c00542f203b586f8f43cd89459
CynicalLawless <darkcetanu@gmail.com>
2024-02-13 14:53:19 +0800

    jh

--- Modal/Modal.js
+++ Modal/Modal.js
@@ -1,16 +1,32 @@
 // Get Modal Variable
+var crt_modal = document.getElementById("crt_modal");
+var snd_modal = document.getElementById("snd_modal");
 var add_modal = document.getElementById("add_modal");
 var dlt_modal = document.getElementById("dlt_modal");
 
 // Get Modal Button Variable
+var crt_btn = document.getElementById("crt");
+var snd_btn = document.getElementById("snd");
 var add_btn = document.getElementById("add");
 var dlt_btn = document.getElementById("dlt");
 
 // Get Span Close Variable
+var crt_span = document.getElementsByClassName("crt_close")[0];
+var snd_span = document.getElementsByClassName("snd_close");
 var add_span = document.getElementsByClassName("add_close")[0];
 var dlt_span = document.getElementsByClassName("dlt_close")[0];
 
 // Open Modal Function
+crt_btn.onclick = function()
+{
+  crt_modal.style.display = "block";
+}
+
+snd_btn.onclick = function()
+{
+  snd_modal.style.display = "block";
+}
+
 add_btn.onclick = function()
 {
   add_modal.style.display = "block";
@@ -22,6 +38,16 @@ dlt_btn.onclick = function()
 }
 
 // Close Modal Function - Button
+crt_span.onclick = function()
+{
+  crt_modal.style.display = "none";
+}
+
+snd_span.onclick = function()
+{
+  snd_modal.style.display = "none";
+}
+
 add_span.onclick = function()
 {
   add_modal.style.display = "none";
@@ -35,6 +61,16 @@ dlt_span.onclick = function()
 // Close Modal Function - Window
 window.onclick = function(event)
 {
+  if (event.target == crt_modal)
+  {
+    crt_modal.style.display = "none";
+  }
+
+  if (event.target == snd_modal)
+  {
+    snd_modal.style.display = "none";
+  }
+
   if (event.target == add_modal)
   {
     add_modal.style.display = "none";
--- PAGES/DOC_VIEW/DOC_VIEW.php
+++ PAGES/DOC_VIEW/DOC_VIEW.php
@@ -5,24 +5,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="DOC_VIEW.css">
+    <link rel="stylesheet" href="../../Modal/Modal.css">
     <link rel="stylesheet" href="../../DOTS_NAVBAR/DOTS_NAV.css">
     <title></title>
 </head>
-<style>
-    table,
-    th,
-    td {
-        border: 1px solid;
-    }
-    table {
-        border-collapse: collapse;
-    }
-
-    tr:hover {
-        background-color: coral;
-    }
-</style>
-
 <body>
      <?php include '../../DOTS_NAVBAR/DOTS_NAV.php';?>
 
@@ -31,120 +17,136 @@
         <div class="main">
 
             <div class="grid">
-                <input type="text" name="searchBar" class="search" id="searchBar">
+                <input type="text" name="searchBar" class="search" id="searchBar" placeholder="Search Document">
 
-                <table class="db_table" id="DOC_VIEW_MAIN" name="DOC_VIEW_MAIN">
-                    <thead>
-                        <tr></tr>
-                    </thead>
-                    <tbody>
+                <div class="tbl_cont">
+                    <table class="db_table" id="DOC_VIEW_MAIN" name="DOC_VIEW_MAIN">
+                        <thead>
+                            <tr></tr>
+                        </thead>
+                        <tbody>
 
-                    </tbody>
-                </table>
+                        </tbody>
+                    </table>
+                </div>
 
                 <div class="opt">
-                    <button name="BTN_DOC_CREATE" id="BTN_DOC_CREATE">CREATE</button>
-                    <button name="BTN_DOC_SEND" id="BTN_DOC_SEND">SEND</button>
-                    <button name="BTN_DOC_ATTACHMENTS" id="BTN_DOC_ATTACHMENTS">ATTACHMENTS</button>
+                    <button class="opt_btn" id="crt" name="BTN_DOC_CREATE" id="BTN_DOC_CREATE">CREATE</button>
+                    <button class="opt_btn" id="snd" name="BTN_DOC_SEND" id="BTN_DOC_SEND">SEND</button>
+                    <button class="opt_btn" id="att" name="BTN_DOC_ATTACHMENTS" id="BTN_DOC_ATTACHMENTS">ATTACHMENTS</button>
                 </div>
             </div>
 
-            <div>
-                <h1>CREATE MODAL</h1>
-                <form action="submit" id="FORM_DOC_RECEIVE">
-                    <div>
-
-                        <label for="ACTION_ID_3">CREATE</label>
-                        <input type="radio" name=ACTION_ID value='3' id="ACTION_ID_3">
-                        <label for="ACTION_ID_2">RECEIVE</label>
-                        <input type="radio" name=ACTION_ID value='2' id="ACTION_ID_2" checked>
-                    </div>
-                    <div>
-                        <label for="CREATE_DOC_NUM">Document Number:</label> <br>
-                        <input type="text" name="DOC_NUM" id="CREATE_DOC_NUM" disabled>
-                    </div>
-                    <div>
-                        <label for="CREATE_FULLNAME">Received By:</label> <br>
-                        <input type="text" name="FULLNAME" id="CREATE_FULLNAME" disabled>
-                    </div>
-                    <div>
-                        <label for="CREATE_DATE_TIME_RECEIVED">Date Received:</label> <br>
-                        <input type="datetime-local" name="DATE_TIME_RECEIVED" id="CREATE_DATE_TIME_RECEIVED">
-                    </div>
-                    <div>
-                        <label for="CREATE_LETTER_DATE">Letter Date:</label> <br>
-                        <input type="date" name="LETTER_DATE" id="CREATE_LETTER_DATE">
-                    </div>
-                    <div>
-                        <label for="CREATE_DOC_TYPE">Document Type:</label> <br>
-                        <select name="DOC_TYPE_ID" id="CREATE_DOC_TYPE">
-                            <option value="" disabled selected>Please Select Type</option>
-                            <!-- to be filled using database -->
-                        </select>
-                    </div>
-                    <div>
-                        <label for="CREATE_DOC_OFFICE">Office/Agency</label> <br>
-                        <select name="S_OFFICE_ID" id="CREATE_DOC_OFFICE">
-                            <option value="" disabled selected>Please Select Office</option>
-                            <!-- to be filled using database -->
-                        </select>
-                    </div>
-                    <div>
-                        <label for="CREATE_DOC_SUBJECT">Subject:</label> <br>
-                        <input type="text" name="DOC_SUBJECT" id="CREATE_DOC_SUBJECT">
+            <div class="modal" id="crt_modal">
+                <div class="modal_content">
+                    <div class="modal_banner">
+                        <span class="crt_close">&times;</span>
+                        <h1>Create</h1>
                     </div>
 
-                    <input type="text" name="DOC_STATUS" id="CREATE_DOC_STATUS" value="1" hidden>
-                    <input type="text" name="R_USER_ID" id="CREATE_R_USER_ID" hidden>
-                    <input type="text" name="R_DEPT_ID" id="CREATE_R_DEPT_ID" hidden>
+                    <form action="submit" id="FORM_DOC_RECEIVE">
+                        <div>
 
-                    <input type="submit" value="Submit">
+                            <label for="ACTION_ID_3">CREATE</label>
+                            <input type="radio" name=ACTION_ID value='3' id="ACTION_ID_3">
+                            <label for="ACTION_ID_2">RECEIVE</label>
+                            <input type="radio" name=ACTION_ID value='2' id="ACTION_ID_2" checked>
+                        </div>
+                        <div>
+                            <label for="CREATE_DOC_NUM">Document Number:</label> <br>
+                            <input type="text" name="DOC_NUM" id="CREATE_DOC_NUM" disabled>
+                        </div>
+                        <div>
+                            <label for="CREATE_FULLNAME">Received By:</label> <br>
+                            <input type="text" name="FULLNAME" id="CREATE_FULLNAME" disabled>
+                        </div>
+                        <div>
+                            <label for="CREATE_DATE_TIME_RECEIVED">Date Received:</label> <br>
+                            <input type="datetime-local" name="DATE_TIME_RECEIVED" id="CREATE_DATE_TIME_RECEIVED">
+                        </div>
+                        <div>
+                            <label for="CREATE_LETTER_DATE">Letter Date:</label> <br>
+                            <input type="date" name="LETTER_DATE" id="CREATE_LETTER_DATE">
+                        </div>
+                        <div>
+                            <label for="CREATE_DOC_TYPE">Document Type:</label> <br>
+                            <select name="DOC_TYPE_ID" id="CREATE_DOC_TYPE">
+                                <option value="" disabled selected>Please Select Type</option>
+                                <!-- to be filled using database -->
+                            </select>
+                        </div>
+                        <div>
+                            <label for="CREATE_DOC_OFFICE">Office/Agency</label> <br>
+                            <select name="S_OFFICE_ID" id="CREATE_DOC_OFFICE">
+                                <option value="" disabled selected>Please Select Office</option>
+                                <!-- to be filled using database -->
+                            </select>
+                        </div>
+                        <div>
+                            <label for="CREATE_DOC_SUBJECT">Subject:</label> <br>
+                            <input type="text" name="DOC_SUBJECT" id="CREATE_DOC_SUBJECT">
+                        </div>
 
-                </form>
+                        <input type="text" name="DOC_STATUS" id="CREATE_DOC_STATUS" value="1" hidden>
+                        <input type="text" name="R_USER_ID" id="CREATE_R_USER_ID" hidden>
+                        <input type="text" name="R_DEPT_ID" id="CREATE_R_DEPT_ID" hidden>
+
+                        <input type="submit" value="Submit">
+
+                    </form>
+                </div>
             </div>
-            <div>
-                <h1>SEND MODAL</h1>
-                <form action="submit" name="FORM_DOC_SEND" id="FORM_DOC_SEND">
-                    <div>
-                        <label for="SEND_DOC_NUM">Document Number:</label>
-                        <input type="text" name="DOC_NUM" id="SEND_DOC_NUM" readonly>
-                    </div>
-                    <div>
-                        <label for="SEND_DATE_TIME_RECEIVED">Date Received:</label>
-                        <input type="datetime-local" name="DATE_TIME_RECEIVED" id="SEND_DATE_TIME_RECEIVED">
-                    </div>
-                    <div>
-                        <label for="SEND_DOC_PRPS">Documnet Purpose:</label>
-                        <select name="PRPS_ID" id="SEND_DOC_PRPS">
-                            <option value="" disabled selected>Please Select Purpose</option>
-                            <!-- to be filled using database -->
-                        </select>
-                    </div>
-                    <div>
-                        <label for="SEND_R_DEPT_ID">Department:</label>
-                        <select name="R_DEPT_ID" id="SEND_R_DEPT_ID">
-                            <option value="" disabled selected>Please Select Department</option>
-                            <!-- to be filled using database -->
-                        </select>
-                    </div>
-                    <div>
-                        <label for="SEND_DOC_ADDRESSEE">Addressee:</label>
-                        <select name="R_USER_ID" id="SEND_DOC_ADDRESSEE">
-                            <option value="" disabled selected>Please Select Addressee</option>
-                            <!-- to be filled using database -->
-                        </select>
+
+
+            <div class="modal" id="snd_modal">
+                <div class="modal_content">
+                    <div class="modal_banner">
+                        <span class="snd_close">&times;</span>
+                        <h1>Send</h1>
                     </div>
 
-                    <div>
-                        <label for="SEND_DOC_NOTES">Notes:</label>
-                        <input type="text" name="DOC_NOTES" id="SEND_DOC_NOTES">
-                    </div>
+                    <form action="submit" name="FORM_DOC_SEND" id="FORM_DOC_SEND">
+                        <div>
+                            <label for="SEND_DOC_NUM">Document Number:</label>
+                            <input type="text" name="DOC_NUM" id="SEND_DOC_NUM" readonly>
+                        </div>
+                        <div>
+                            <label for="SEND_DATE_TIME_RECEIVED">Date Received:</label>
+                            <input type="datetime-local" name="DATE_TIME_RECEIVED" id="SEND_DATE_TIME_RECEIVED">
+                        </div>
+                        <div>
+                            <label for="SEND_DOC_PRPS">Documnet Purpose:</label>
+                            <select name="PRPS_ID" id="SEND_DOC_PRPS">
+                                <option value="" disabled selected>Please Select Purpose</option>
+                                <!-- to be filled using database -->
+                            </select>
+                        </div>
+                        <div>
+                            <label for="SEND_R_DEPT_ID">Department:</label>
+                            <select name="R_DEPT_ID" id="SEND_R_DEPT_ID">
+                                <option value="" disabled selected>Please Select Department</option>
+                                <!-- to be filled using database -->
+                            </select>
+                        </div>
+                        <div>
+                            <label for="SEND_DOC_ADDRESSEE">Addressee:</label>
+                            <select name="R_USER_ID" id="SEND_DOC_ADDRESSEE">
+                                <option value="" disabled selected>Please Select Addressee</option>
+                                <!-- to be filled using database -->
+                            </select>
+                        </div>
 
-                    <input type="text" name="ACTION_ID" id="SEND_DOC_ACTION" value="3">
-                    <input type="text" name="S_USER_ID" id="SEND_S_USER_ID" value="">
+                        <div>
+                            <label for="SEND_DOC_NOTES">Notes:</label>
+                            <input type="text" name="DOC_NOTES" id="SEND_DOC_NOTES">
+                        </div>
 
-                    <input type="submit" value="Send">
-                </form>
+                        <input type="text" name="ACTION_ID" id="SEND_DOC_ACTION" value="3">
+                        <input type="text" name="S_USER_ID" id="SEND_S_USER_ID" value="">
+
+                        <input type="submit" value="Send">
+                    </form>
+                </div>
             </div>
 
             <div>
@@ -155,6 +157,7 @@
     </div>
 </body>
 
+<script src="../../Modal/Modal.js"></script>
 <script src="../../SCRIPTS/Constants.js"></script>
 <script src="./script.js" type="module"></script>
 
