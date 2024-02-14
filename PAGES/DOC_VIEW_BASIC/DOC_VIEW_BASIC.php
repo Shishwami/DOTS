76c4125d4e954b1eacabee2ba6c6bb1ceb343e91
Shishwami <157940784+Shishwami@users.noreply.github.com>
2024-02-14 09:22:08 +0800

    Update DOC_VIEW_BASIC.php

--- PAGES/DOC_VIEW_BASIC/DOC_VIEW_BASIC.php
+++ PAGES/DOC_VIEW_BASIC/DOC_VIEW_BASIC.php
@@ -24,13 +24,18 @@
 
     tr:hover {
         background-color: coral;
-    }   
+    }
 </style>
 
 <body>
 
-
-    <input type="text" name="searchBar" id="searchBar">
+    <div>
+        <input type="text" name="searchBar" id="searchBar">
+        <label for="RADIO_RECEIVE">Receive</label>
+        <input type="radio" name="ACTION_TYPE" id="RADIO_RECEIVE" value="receive" checked>
+        <label for="RADIO_SEND">Send</label>
+        <input type="radio" name="ACTION_TYPE" id="RADIO_SEND" value="send">
+    </div>
 
     <table id="DOC_VIEW_BASIC" name="DOC_VIEW_BASIC">
         <thead>
@@ -40,6 +45,9 @@
 
         </tbody>
     </table>
+
+    <button type="button" id="R_BTN">Receive</button>
+    <button type="button" id="S_BTN">SEND</button>
 
 </body>
 
