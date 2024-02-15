e209ae6b5bf83801e05eac3be83a7367ace6d6a1
CynicalLawless <darkcetanu@gmail.com>
2024-02-15 10:59:06 +0800

    CXVBN

--- PAGES/DOC_VIEW/DOC_VIEW.php
+++ PAGES/DOC_VIEW/DOC_VIEW.php
@@ -48,53 +48,74 @@
                     </div>
 
                     <form class="form" action="submit" id="FORM_DOC_RECEIVE">
-                        <div>
-
-                            <label for="ACTION_ID_3">CREATE</label>
-                            <input type="radio" name=ACTION_ID value='3' id="ACTION_ID_3">
-                            <label for="ACTION_ID_2">RECEIVE</label>
-                            <input type="radio" name=ACTION_ID value='2' id="ACTION_ID_2" checked>
-                        </div>
-                        <div>
-                            <label for="CREATE_DOC_NUM">Document Number:</label> <br>
-                            <input type="text" name="DOC_NUM" id="CREATE_DOC_NUM" disabled>
-                        </div>
-                        <div>
-                            <label for="CREATE_FULLNAME">Received By:</label> <br>
-                            <input type="text" name="FULLNAME" id="CREATE_FULLNAME" disabled>
-                        </div>
-                        <div>
-                            <label for="CREATE_DATE_TIME_RECEIVED">Date Received:</label> <br>
-                            <input type="datetime-local" name="DATE_TIME_RECEIVED" id="CREATE_DATE_TIME_RECEIVED">
-                        </div>
-                        <div>
-                            <label for="CREATE_LETTER_DATE">Letter Date:</label> <br>
-                            <input type="date" name="LETTER_DATE" id="CREATE_LETTER_DATE">
-                        </div>
-                        <div>
-                            <label for="CREATE_DOC_TYPE">Document Type:</label> <br>
-                            <select name="DOC_TYPE_ID" id="CREATE_DOC_TYPE">
-                                <option value="" disabled selected>Please Select Type</option>
-                                <!-- to be filled using database -->
-                            </select>
-                        </div>
-                        <div>
-                            <label for="CREATE_DOC_OFFICE">Office/Agency</label> <br>
-                            <select name="S_OFFICE_ID" id="CREATE_DOC_OFFICE">
-                                <option value="" disabled selected>Please Select Office</option>
-                                <!-- to be filled using database -->
-                            </select>
-                        </div>
-                        <div>
-                            <label for="CREATE_DOC_SUBJECT">Subject:</label> <br>
-                            <input type="text" name="DOC_SUBJECT" id="CREATE_DOC_SUBJECT">
+                        <div class="radio_btn">
+                            <div class="btn1">
+                                <label class="radio_cnt" for="ACTION_ID_3">CREATE
+                                <input type="radio" name=ACTION_ID value='3' id="ACTION_ID_3">
+                                <span class="r_btn one"></span>
+                                </label>
+                            </div>
+                            <div class="btn2">
+                                <label class="radio_cnt" for="ACTION_ID_2">RECEIVE
+                                <input type="radio" name=ACTION_ID value='2' id="ACTION_ID_2" checked>
+                                <span class="r_btn two"></span>
+                                </label>  
+                            </div>
                         </div>
 
-                        <input type="text" name="DOC_STATUS" id="CREATE_DOC_STATUS" value="1" hidden>
-                        <input type="text" name="R_USER_ID" id="CREATE_R_USER_ID" hidden>
-                        <input type="text" name="R_DEPT_ID" id="CREATE_R_DEPT_ID" hidden>
+                        <div class="form_content">
+                            <div class="box content_one">
+                                <div>
+                                    <label for="CREATE_DOC_NUM">Document Number:</label>
+                                    <input type="text" name="DOC_NUM" id="CREATE_DOC_NUM" disabled>
+                                </div>
+                                <div>
+                                    <label for="CREATE_FULLNAME">Received By:</label>
+                                    <input type="text" name="FULLNAME" id="CREATE_FULLNAME" disabled>
+                                </div>
+                            </div>
 
-                        <input type="submit" value="Submit">
+                            <div class=" box content_two">
+                                <div>
+                                    <label for="CREATE_DATE_TIME_RECEIVED">Date Received:</label>
+                                    <input type="datetime-local" name="DATE_TIME_RECEIVED" id="CREATE_DATE_TIME_RECEIVED">
+                                </div>
+                                <div>
+                                    <label for="CREATE_LETTER_DATE">Letter Date:</label>
+                                    <input type="date" name="LETTER_DATE" id="CREATE_LETTER_DATE">
+                                </div>
+                            </div>
+                            
+                            <div class="box content_three">
+                                <div>
+                                    <label for="CREATE_DOC_TYPE">Document Type:</label>
+                                    <select name="DOC_TYPE_ID" id="CREATE_DOC_TYPE">
+                                        <option value="" disabled selected>Please Select Type</option>
+                                        <!-- to be filled using database -->
+                                    </select>
+                                </div>
+                                <div>
+                                    <label for="CREATE_DOC_OFFICE">Office/Agency</label>
+                                    <select name="S_OFFICE_ID" id="CREATE_DOC_OFFICE">
+                                        <option value="" disabled selected>Please Select Office</option>
+                                        <!-- to be filled using database -->
+                                    </select>
+                                </div>
+                                <div>
+                                    <label for="CREATE_DOC_SUBJECT">Subject:</label>
+                                    <!-- <input type="text" name="DOC_SUBJECT" id="CREATE_DOC_SUBJECT"> -->
+                                    <textarea name="DOC_SUBJECT" id="DOC_SUBJECT" cols="20" rows="5"></textarea>
+                                </div>
+
+                                <input type="text" name="DOC_STATUS" id="CREATE_DOC_STATUS" value="1" hidden>
+                                <input type="text" name="R_USER_ID" id="CREATE_R_USER_ID" hidden>
+                                <input type="text" name="R_DEPT_ID" id="CREATE_R_DEPT_ID" hidden>
+                            </div>
+                        </div>
+
+                        <div class="submit">
+                            <input type="submit" value="Submit">
+                        </div>
 
                     </form>
                 </div>
@@ -109,50 +130,66 @@
                     </div>
 
                     <form class="form" action="submit" name="FORM_DOC_SEND" id="FORM_DOC_SEND">
-                        <div>
-                            <label for="SEND_DOC_NUM">Document Number:</label>
-                            <input type="text" name="DOC_NUM" id="SEND_DOC_NUM" readonly>
-                        </div>
-                        <div>
-                            <label for="SEND_DATE_TIME_RECEIVED">Date Received:</label>
-                            <input type="datetime-local" name="DATE_TIME_RECEIVED" id="SEND_DATE_TIME_RECEIVED">
-                        </div>
-                        <div>
-                            <label for="SEND_DOC_PRPS">Documnet Purpose:</label>
-                            <select name="PRPS_ID" id="SEND_DOC_PRPS">
-                                <option value="" disabled selected>Please Select Purpose</option>
-                                <!-- to be filled using database -->
-                            </select>
-                        </div>
-                        <div>
-                            <label for="SEND_R_DEPT_ID">Department:</label>
-                            <select name="R_DEPT_ID" id="SEND_R_DEPT_ID">
-                                <option value="" disabled selected>Please Select Department</option>
-                                <!-- to be filled using database -->
-                            </select>
-                        </div>
-                        <div>
-                            <label for="SEND_DOC_ADDRESSEE">Addressee:</label>
-                            <select name="R_USER_ID" id="SEND_DOC_ADDRESSEE">
-                                <option value="" disabled selected>Please Select Addressee</option>
-                                <!-- to be filled using database -->
-                            </select>
+                        <div class="form_content">
+                            <div class="box content_one">
+                                <div>
+                                    <label for="SEND_DOC_NUM">Document Number:</label>
+                                    <input type="text" name="DOC_NUM" id="SEND_DOC_NUM" readonly>
+                                </div>
+                                <div>
+                                    <label for="SEND_DATE_TIME_RECEIVED">Date Received:</label>
+                                    <input type="datetime-local" name="DATE_TIME_RECEIVED" id="SEND_DATE_TIME_RECEIVED">
+                                </div>
+                            </div>
+
+                            <div class="box content_two">
+                                <div>
+                                    <label for="SEND_DOC_PRPS">Documnet Purpose:</label>
+                                    <select name="PRPS_ID" id="SEND_DOC_PRPS">
+                                        <option value="" disabled selected>Please Select Purpose</option>
+                                        <!-- to be filled using database -->
+                                    </select>
+                                </div>
+                                <div>
+                                    <label for="SEND_R_DEPT_ID">Department:</label>
+                                    <select name="R_DEPT_ID" id="SEND_R_DEPT_ID">
+                                        <option value="" disabled selected>Please Select Department</option>
+                                        <!-- to be filled using database -->
+                                    </select>
+                                </div>
+                                <div>
+                                    <label for="SEND_DOC_ADDRESSEE">Addressee:</label>
+                                    <select name="R_USER_ID" id="SEND_DOC_ADDRESSEE">
+                                        <option value="" disabled selected>Please Select Addressee</option>
+                                        <!-- to be filled using database -->
+                                    </select>
+                                </div>
+                            </div>
+
+                            <div class="box content_three">
+                                <div>
+                                    <label for="SEND_DOC_NOTES">Notes:</label>
+                                    <input type="text" name="DOC_NOTES" id="SEND_DOC_NOTES">
+                                </div>
+                                <div>
+                                    <label for="SEND_DOC_ACTION">Action ID</label>
+                                    <input type="text" name="ACTION_ID" id="SEND_DOC_ACTION" value="3">
+                                </div>
+                                <div>
+                                    <label for="SEND_S_USER_ID">User ID</label>
+                                    <input type="text" name="S_USER_ID" id="SEND_S_USER_ID" value="">
+                                </div>
+                            </div>
                         </div>
 
-                        <div>
-                            <label for="SEND_DOC_NOTES">Notes:</label>
-                            <input type="text" name="DOC_NOTES" id="SEND_DOC_NOTES">
+                        <div class="submit">
+                            <input type="submit" value="Send">
                         </div>
-
-                        <input type="text" name="ACTION_ID" id="SEND_DOC_ACTION" value="3">
-                        <input type="text" name="S_USER_ID" id="SEND_S_USER_ID" value="">
-
-                        <input type="submit" value="Send">
                     </form>
                 </div>
             </div>
 
-            <div class="modal" id="atc_modal">
+            <div class="modall" id="atc_modal">
                 <div class="modal_content">
                     <div class="modal_banner">
                         <span class="atc_close">&times;</span>
