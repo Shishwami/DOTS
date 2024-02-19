da0915b74ed039e416a724d08a416598ca92d2b0
Shishwami <157940784+Shishwami@users.noreply.github.com>
2024-02-19 22:03:34 +0800

    Create DB_Constants.php

---
+++ SCRIPTS/DB_Constants.php
@@ -0,0 +1,121 @@
+<?php
+
+const _RESET_TIME = 1000;
+
+const DOTS_DOCUMENT = [
+    'NAME' => 'DOTS_DOCUMENT',
+    'ID' => 'ID',
+    'DOC_NUM' => 'DOC_NUM',
+    'ROUTE_NUM' => 'ROUTE_NUM',
+    'ROUTED' => 'ROUTED',
+    'DOC_SUBJECT' => 'DOC_SUBJECT',
+    'DOC_NOTES' => 'DOC_NOTES',
+    'DOC_TYPE_ID' => 'DOC_TYPE_ID',
+    'LETTER_DATE' => 'LETTER_DATE',
+    'S_OFFICE_ID' => 'S_OFFICE_ID',
+    'S_DEPT_ID' => 'S_DEPT_ID',
+    'S_USER_ID' => 'S_USER_ID',
+    'R_OFFICE_ID' => 'R_OFFICE_ID',
+    'R_USER_ID' => 'R_USER_ID',
+    'R_DEPT_ID' => 'R_DEPT_ID',
+    'ACTION_ID' => 'ACTION_ID',
+    'DATE_TIME_RECEIVED' => 'DATE_TIME_RECEIVED',
+    'DOC_STATUS' => 'DOC_STATUS',
+];
+
+const DOTS_DOC_LOGS = [
+    'NAME' => 'DOTS_DOC_LOGS',
+    'DOC_NUM' => 'DOC_NUM',
+    'ID' => 'ID',
+    'DOC_LOCATION' => 'DOC_LOCATION',
+    'DOC_OPERATION' => 'DOC_OPERATION',
+    'DOC_ADDRESSEE' => 'DOC_ADDRESSEE',
+    'DOC_STATUS' => 'DOC_STATUS',
+];
+
+const DOTS_DOC_OFFICE = [
+    'NAME' => 'DOTS_DOC_OFFICE',
+    'ID' => 'ID',
+    'DOC_OFFICE' => 'DOC_OFFICE',
+];
+
+const DOTS_DOC_ACTION = [
+    'NAME' => 'DOTS_DOC_ACTION',
+    'ID' => 'ID',
+    'DOC_ACTION' => 'DOC_ACTION',
+];
+
+const DOTS_DOC_DEPT = [
+    'NAME' => 'DOTS_DOC_DEPT',
+    'ID' => 'ID',
+    'DOC_DEPT' => 'DOC_DEPT',
+];
+
+const DOTS_DOC_TYPE = [
+    'NAME' => 'DOTS_DOC_TYPE',
+    'ID' => 'ID',
+    'DOC_TYPE' => 'DOC_TYPE',
+];
+
+const DOTS_DOC_PRPS = [
+    'NAME' => 'DOTS_DOC_PRPS',
+    'ID' => 'ID',
+    'DOC_PRPS' => 'DOC_PRPS',
+];
+
+const DOTS_DOC_STATUS = [
+    'NAME' => 'DOTS_DOC_STATUS',
+    'ID' => 'ID',
+    'DOC_STATUS' => 'DOC_STATUS',
+];
+
+const DOTS_ACCOUNT_INFO = [
+    'NAME' => 'DOTS_ACCOUNT_INFO',
+    'HRIS_ID' => 'HRIS_ID',
+    'FULL_NAME' => 'FULL_NAME',
+    'INITIAL' => 'INITIAL',
+    'OFFICE_ID' => 'OFFICE_ID',
+    'DEPT_ID' => 'DEPT_ID',
+    'USERNAME' => 'USERNAME',
+    'PASSWORD' => 'PASSWORD',
+    'DIVISION' => 'DIVISION',
+];
+
+const _REQUEST = [
+    'INSERT' => 'INSERT',
+    'SELECT' => 'SELECT',
+    'UPDATE' => 'UPDATE',
+    'DELETE' => 'DELETE',
+    'GET_DATE' => 'GET_DATE',
+    'GET_DOC_NUM' => 'GET_DOC_NUM',
+    'CREATE_SESSION' => 'CREATE_SESSION',
+    'INSERT_DOCLOG' => 'INSERT_DOCLOG',
+    'GET_SESSION_NAME' => 'GET_SESSION_NAME',
+    'GET_SESSION_INITIAL' => 'GET_SESSION_INITIAL',
+    'GET_SESSION_HRIS_ID' => 'GET_SESSION_HRIS_ID',
+    'GET_SESSION_DEPT_ID' => 'GET_SESSION_DEPT_ID',
+];
+
+const DOTS_DOCUMENT_SUB = [
+    'NAME' => 'DOTS_DOCUMENT_SUB',
+    'DOC_NUM' => 'DOC_NUM',
+    'ID' => 'ID',
+    'PRPS_ID' => 'PRPS_ID',
+    'DOC_NOTES' => 'DOC_NOTES',
+    'S_OFFICE_ID' => 'S_OFFICE_ID',
+    'S_USER_ID' => 'S_USER_ID',
+    'S_DEPT_ID' => 'S_DEPT_ID',
+    'R_OFFICE_ID' => 'R_OFFICE_ID',
+    'R_USER_ID' => 'R_USER_ID',
+    'R_DEPT_ID' => 'R_DEPT_ID',
+    'ACTION_ID' => 'ACTION_ID',
+    'DATE_TIME_RECEIVED' => 'DATE_TIME_RECEIVED',
+];
+
+foreach ([$\DOTS_DOCUMENT, DOTS_DOC_LOGS, DOTS_DOC_OFFICE] as $array) {
+    $keys = array_keys($array);
+    foreach ($keys as $key) {
+        define(strtoupper($key), $key);
+    }
+}
+?>
