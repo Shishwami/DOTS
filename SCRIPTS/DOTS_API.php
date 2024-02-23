0fff03273da27c1a3c18fcc87673271eb66b51e8
Shishwami <157940784+Shishwami@users.noreply.github.com>
2024-02-23 14:39:46 +0800

    Revert "Update DOTS_API.php"
    This reverts commit e7768da9cec46049bd3262f0093c35a3a5b3925e.

--- SCRIPTS/DOTS_API.php
+++ SCRIPTS/DOTS_API.php
@@ -91,9 +91,6 @@ try {
         case 'RECEIVE_DOC_USER':
             receiveDocUser($inputs, $conn);
             break;
-        case 'SED_DOC_USER':
-            sendDocUser($inputs,$conn);
-            break;
     }
     $conn->close();
 
@@ -246,11 +243,11 @@ function userLogin($inputs, $conn)
         // if($_SESSION['DOTS_PRIV'] == 0){
         //     //
         // }
-
+        
         // if($_SESSION['DOTS_PRIV'] == 1){
         //     //
         // }
-
+        
         // if($_SESSION['DOTS_PRIV'] == 2){
         //     //
         // }
