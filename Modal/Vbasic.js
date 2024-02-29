866efe400cacef0ebcd8689f0dc4b35ffadd1300
Shishwami <157940784+Shishwami@users.noreply.github.com>
2024-02-29 10:29:20 +0800

    Update Vbasic.js

--- Modal/Vbasic.js
+++ Modal/Vbasic.js
@@ -2,31 +2,21 @@ const rec_span = document.getElementsByClassName("rec_close")[0];
 const sent_span = document.getElementsByClassName("sent_close")[0];
 
 if (rec_span) {
-    rec_span.onclick = function()
-    {
-    rec_modal.style.display = "none";
-    }
-  }
-
-window.onclick = function(event)
-{
-  if (event.target == rec_modal)
-  {
+  rec_span.onclick = function () {
     rec_modal.style.display = "none";
   }
 }
-
 if (sent_span) {
-  sent_span.onclick = function()
-  {
-  sent_modal.style.display = "none";
+  sent_span.onclick = function () {
+    sent_modal.style.display = "none";
   }
 }
 
-window.onclick = function(event)
-{
-if (event.target == sent_modal)
-{
-  sent_modal.style.display = "none";
-}
+window.onclick = function (event) {
+  if (event.target == sent_modal) {
+    sent_modal.style.display = "none";
+  }
+  if (event.target == rec_modal) {
+    rec_modal.style.display = "none";
+  }
 }
