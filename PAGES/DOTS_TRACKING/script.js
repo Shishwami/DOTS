6bb9967911953a91727165b8457fed15b0039e36
Shishwami <157940784+Shishwami@users.noreply.github.com>
2024-03-01 10:24:45 +0800

    Update script.js

--- PAGES/DOTS_TRACKING/script.js
+++ PAGES/DOTS_TRACKING/script.js
@@ -8,13 +8,7 @@ const DOC_VIEW_TRACKING=document.getElementById("DOC_VIEW_TRACKING");
 
 
 function setTable(filter){
-    var data = {};
-    if (action_type == 'receive') {
-        data['REQUEST'] = _REQUEST.GET_TABLE_INBOUND;
-    }
-    if (action_type == 'send') {
-        data['REQUEST'] = _REQUEST.GET_TABLE_OUTBOUND;
-    }
+
 
     console.log(data);
 
