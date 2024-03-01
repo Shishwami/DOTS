3eca5d22ce43765171528d00e283db9f706dea4f
Shishwami <157940784+Shishwami@users.noreply.github.com>
2024-03-01 10:23:51 +0800

    Update script.js

--- PAGES/DOTS_TRACKING/script.js
+++ PAGES/DOTS_TRACKING/script.js
@@ -1,3 +1,14 @@
 const searchBar = document.getElementById("searchBar");
 
 const DOC_VIEW_TRACKING=document.getElementById("DOC_VIEW_TRACKING");
+
+
+
+
+
+
+function setTable(filter){
+    
+}
+
+
