bf9067ad21bc6c88e02ab743c9926fb8f15f09e5
Shishwami <157940784+Shishwami@users.noreply.github.com>
2024-03-01 10:24:49 +0800

    Update script.js

--- PAGES/DOTS_TRACKING/script.js
+++ PAGES/DOTS_TRACKING/script.js
@@ -10,8 +10,6 @@ const DOC_VIEW_TRACKING=document.getElementById("DOC_VIEW_TRACKING");
 function setTable(filter){
 
 
-    console.log(data);
-
     MyAjax.createJSON((error, response) => {
         if (error) {
             alert(error);
