371221e1842cac75ca5ec84aae08321caceed830
Shishwami <157940784+Shishwami@users.noreply.github.com>
2024-03-01 10:24:34 +0800

    Update script.js

--- PAGES/DOTS_TRACKING/script.js
+++ PAGES/DOTS_TRACKING/script.js
@@ -8,7 +8,38 @@ const DOC_VIEW_TRACKING=document.getElementById("DOC_VIEW_TRACKING");
 
 
 function setTable(filter){
-    
+    var data = {};
+    if (action_type == 'receive') {
+        data['REQUEST'] = _REQUEST.GET_TABLE_INBOUND;
+    }
+    if (action_type == 'send') {
+        data['REQUEST'] = _REQUEST.GET_TABLE_OUTBOUND;
+    }
+
+    console.log(data);
+
+    MyAjax.createJSON((error, response) => {
+        if (error) {
+            alert(error);
+        } else {
+
+        }
+        // const thead = DOC_VIEW_BASIC.querySelector('thead');
+        // const tbody = DOC_VIEW_BASIC.querySelector('tbody');
+        // if (response.THEAD) {
+        //     thead.innerHTML = response.THEAD;
+        // } else {
+        //     thead.innerHTML = '';
+        // }
+        // if (response.TBODY) {
+        //     tbody.innerHTML = response.TBODY;
+        // } else {
+        //     tbody.innerHTML = '';
+        // }
+        JsFunctions.updateTable(DOC_VIEW_BASIC, response.RESULT, response.BUTTONS, filter);
+        setButtons(DOC_VIEW_BASIC);
+    }, data);
+
 }
 
 
