065bc22451344310ae8c22210902f1f79b14d992
Shishwami <157940784+Shishwami@users.noreply.github.com>
2024-03-01 10:21:46 +0800

    Update DOTS_TRACKING.php

--- PAGES/DOTS_TRACKING/DOTS_TRACKING.php
+++ PAGES/DOTS_TRACKING/DOTS_TRACKING.php
@@ -1,13 +1,15 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
-    
-<input type="text">
+
+    <input type="text" id="searchBar">
 
     <table id="VIEW_TRACKING">
         <thead>
@@ -21,4 +23,5 @@
 
 
 </body>
+
 </html>
