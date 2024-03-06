108d0532807648f2f2cc02c4a1103aab3e2c59f7
Shishwami <157940784+Shishwami@users.noreply.github.com>
2024-03-06 10:36:03 +0800

    Update DOTS_NAV.php

--- DOTS_NAVBAR/DOTS_NAV.php
+++ DOTS_NAVBAR/DOTS_NAV.php
@@ -5,10 +5,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title></title>
-    <link rel="stylesheet" href="../CSS/DOTS_NAV.css">
+    <!-- <link rel="stylesheet" href="../CSS/DOTS_NAV.css">
     <link rel="stylesheet" href="../CSS/FontAwesome/css/all.css">
     <link rel="stylesheet" href="../CSS/FontAwesome/css/fontawesome.css">
-    <link rel="stylesheet" href="../CSS/FontAwesome/css/fontawesome.min.css">
+    <link rel="stylesheet" href="../CSS/FontAwesome/css/fontawesome.min.css"> -->
     <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/> -->
 </head>
 <body>
