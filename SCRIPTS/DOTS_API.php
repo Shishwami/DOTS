956f1dc30c0ad4ce7a4980b309c5a7fd82759cfb
Shishwami <157940784+Shishwami@users.noreply.github.com>
2024-03-06 08:19:56 +0800

    Update DOTS_API.php

--- SCRIPTS/DOTS_API.php
+++ SCRIPTS/DOTS_API.php
@@ -1294,10 +1294,8 @@ function getTableTracking($inputs, $conn)
                 IF(DOTS_ACCOUNT_INFO.DEPT_ID IS NOT NULL,CONCAT(DOTS_DOC_DEPT.DOC_DEPT,'-'), ' '), 
                 IFNULL(DOTS_ACCOUNT_INFO.FULL_NAME, ' ')) as 'Location'",
 
-
             'DATE_TIME_ACTION as `Date of Action`',
             'DOTS_DOC_ACTION.DOC_ACTION as `Action`',
-
         ],
         'JOIN' => [
             [
