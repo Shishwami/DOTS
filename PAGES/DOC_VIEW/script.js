c8579a0ce718bb8a6861bebcfe63319e476fc023
Shishwami <157940784+Shishwami@users.noreply.github.com>
2024-03-07 16:21:39 +0800

    Update script.js

--- PAGES/DOC_VIEW/script.js
+++ PAGES/DOC_VIEW/script.js
@@ -93,11 +93,7 @@ function InitializePAGE() {
     if (BTN_ATTACH_INS) {
         BTN_ATTACH_INS.addEventListener("click", function () {
             var elements = document.getElementsByClassName('img-zoom-lens');
-
-            // Convert the HTMLCollection to an array to use array methods
             var elementsArray = Array.from(elements);
-
-            // Iterate over the array of elements and remove each one
             elementsArray.forEach(function (element) {
                 element.parentNode.removeChild(element);
             });
