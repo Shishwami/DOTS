c7451a6a7cd35654fde79443e0a802d697f6f4ba
Shishwami <157940784+Shishwami@users.noreply.github.com>
2024-03-08 08:33:29 +0800

    Update DOC_VIEW_BASIC.php

--- PAGES/DOC_VIEW_BASIC/DOC_VIEW_BASIC.php
+++ PAGES/DOC_VIEW_BASIC/DOC_VIEW_BASIC.php
@@ -7,6 +7,7 @@
     <link rel="stylesheet" href="../../CSS/Modal.css">
     <link rel="stylesheet" href="../../CSS/DOTS_NAV.css">
     <link rel="stylesheet" href="../../CSS/DOC_VIEW_BASIC.css">
+    <link rel="stylesheet" href="../../CSS/Notifications.css">
     <!-- <link rel="stylesheet" href="../../CSS/test.css"> -->
     <link rel="stylesheet" href="../../CSS/FontAwesome/css/all.css">
     <link rel="stylesheet" href="../../CSS/FontAwesome/css/fontawesome.css">
@@ -325,6 +326,9 @@
         </div>
     </div>
     <br>
+    <?php
+    include "../Notifications/Notifications.php";
+    ?>
 </body>
 
 <script src="../../Modal/Vbasic.js"></script>
