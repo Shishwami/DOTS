8b01f5ff6a56279bff427a6807744f8dc2f1a9d4
Shishwami <157940784+Shishwami@users.noreply.github.com>
2024-03-08 08:59:01 +0800

    Update script.js

--- PAGES/DOC_VIEW_BASIC/script.js
+++ PAGES/DOC_VIEW_BASIC/script.js
@@ -21,13 +21,6 @@ const SEND_S_USER_ID = FORM_DOC_SEND.querySelector('#SEND_S_USER_ID');
 const SEND_S_DEPT_ID = FORM_DOC_SEND.querySelector('#SEND_S_DEPT_ID');
 const SEND_R_DEPT_ID = FORM_DOC_SEND.querySelector('#SEND_R_DEPT_ID');
 
-// const FORM_DOC_EDIT = document.getElementById('FORM_DOC_EDIT');
-// const EDIT_DATE_TIME_EDIT = FORM_DOC_EDIT.querySelector("#EDIT_DATE_TIME_EDIT");
-// const EDIT_DOC_PRPS = FORM_DOC_EDIT.querySelector("#EDIT_DOC_PRPS");
-// const EDIT_DOC_ID = FORM_DOC_EDIT.querySelector('#EDIT_DOC_ID');
-// const EDIT_S_USER_ID = FORM_DOC_EDIT.querySelector('#EDIT_S_USER_ID');
-// const EDIT_S_DEPT_ID = FORM_DOC_EDIT.querySelector('#EDIT_S_DEPT_ID');
-// const EDIT_R_DEPT_ID = FORM_DOC_EDIT.querySelector('#EDIT_R_DEPT_ID');
 const FORM_DOC_CANCEL_R = document.getElementById("FORM_DOC_CANCEL_R");
 const CANCEL_R_NOTES = FORM_DOC_CANCEL_R.querySelector("#CANCEL_R_NOTES");
 const CANCEL_R_DEPT = FORM_DOC_CANCEL_R.querySelector("CANCEL_R_DEPT");
@@ -36,8 +29,6 @@ const CANCEL_R_ID = FORM_DOC_CANCEL_R.querySelector("#CANCEL_R_ID");
 const FORM_DOC_CANCEL_S = document.getElementById("FORM_DOC_CANCEL_S");
 const CANCEL_S_NOTES = FORM_DOC_CANCEL_S.querySelector("#CANCEL_S_NOTES");
 const CANCEL_S_ID = FORM_DOC_CANCEL_S.querySelector("#CANCEL_S_ID");
-
-
 
 const RADIO_SEND = document.getElementById("RADIO_SEND");
 const RADIO_RECEIVE = document.getElementById("RADIO_RECEIVE");
@@ -54,7 +45,6 @@ let action_type = "receive";
 // const ATTACH_FILE = document.getElementById("ATTACH_FILE");
 // const ATTACH_RESULTS = document.getElementById("ATTACH_RESULTS");
 // const ATTACH_ZOOM = document.getElementById("ATTACH_ZOOM");
-
 
 setSession();
 setFormEvents();
