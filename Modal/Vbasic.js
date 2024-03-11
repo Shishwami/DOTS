74bb2236d5a0a841d1a0b4d66d7a6210c9c22964
CynicalLawless <darkcetanu@gmail.com>
2024-03-11 15:59:49 +0800

    dfghjhgf

--- Modal/Vbasic.js
+++ Modal/Vbasic.js
@@ -11,6 +11,7 @@ const s_cnl_span = document.getElementsByClassName("s_cnl_close")[0];
 const atc_span = document.getElementsByClassName("atc_close")[0];
 const ins_sub_span = document.getElementsByClassName("ins_sub_close")[0];
 const atc_sub_span = document.getElementsByClassName("atc_sub_close")[0];
+const track_span = document.getElementsByClassName("track_close")[0];
 
 if (ins_sub_btn) {
   ins_sub_btn.onclick = function()
@@ -68,6 +69,12 @@ if (atc_sub_span) {
   }
 }
 
+if (track_span) {
+  track_span.onclick = function () {
+    track_modal.style.display = "none";
+  }
+}
+
 window.onclick = function (event) {
   if (event.target == sent_modal) {
     sent_modal.style.display = "none";
@@ -95,5 +102,9 @@ window.onclick = function (event) {
 
   if (event.target == atc_submodal) {
     atc_submodal.style.display = "none";
+  }
+
+  if (event.target == track_modal) {
+    track_modal.style.display = "none";
   }
 }
