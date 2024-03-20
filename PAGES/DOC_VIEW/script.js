ef1f10528faf681bded0865726608423a612389d
Shishwami <157940784+Shishwami@users.noreply.github.com>
2024-03-20 08:33:16 +0800

    Update script.js

--- PAGES/DOC_VIEW/script.js
+++ PAGES/DOC_VIEW/script.js
@@ -287,9 +287,12 @@ function setPrinting(id, doc_num, route_num) {
 
         doc_purp.innerHTML = "";
         var counter = 0;
+        var max = Math.floor(response.PRPS.length / 2);
+
         var container = document.createElement('div'); // Create a container to group checkboxes
         container.className = "column";
         response.PRPS.forEach(function (item) {
+            console.log(response.PRPS.length);
             var id = item.ID;
             var docPrps = item.DOC_PRPS;
 
@@ -311,7 +314,7 @@ function setPrinting(id, doc_num, route_num) {
             container.appendChild(document.createElement('br')); // Add line break
 
             counter++;
-            if (counter % 5 === 0) { // Check if 5 checkboxes are added
+            if (counter % max === 0) { // Check if 5 checkboxes are added
                 doc_purp.appendChild(container); // Add container to the main document
                 container = document.createElement('div'); // Reset container for next group
                 container.className = "column";
