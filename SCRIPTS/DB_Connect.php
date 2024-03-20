55d76f3359eadba22c3d3f855be11a798725ed16
Shishwami <157940784+Shishwami@users.noreply.github.com>
2024-03-20 15:49:02 +0800

    Update DB_Connect.php

--- SCRIPTS/DB_Connect.php
+++ SCRIPTS/DB_Connect.php
@@ -18,6 +18,10 @@ if ($conn->connect_error) {
     die ("Connection failed: " . $conn->connect_error);
 }
 
+$pdo = new PDO("mysql:host=$servername;dbname=$database", $username, $password);
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+
 function getPdoConnection()
 {
 
