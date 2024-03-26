00a1cf4ff9e302abfa330f275418ae01b9549196
Shishwami <157940784+Shishwami@users.noreply.github.com>
2024-03-26 14:36:21 +0800

    Update JsFunctions.js

--- SCRIPTS/JsFunctions.js
+++ SCRIPTS/JsFunctions.js
@@ -1,21 +1,31 @@
 class JsFunctions {
 
+    /**
+    * Updates the content of a table with new data.
+    * @param {Element} table - The table element to update.
+    * @param {Array} results - An array of objects containing the data to populate the table.
+    * @param {Array} buttons - An array of objects representing buttons to include in each row.
+    * @param {string} filter - A string used to filter rows based on their content.
+    */
     static updateTable(table, results, buttons, filter) {
-        const tbody = table.querySelector('tbody');
-        const thead = table.querySelector('thead');
+        const tbody = table.querySelector('tbody'); // Reference to the table body element
+        const thead = table.querySelector('thead'); // Reference to the table header element
 
-        tbody.innerHTML = '';
-        thead.innerHTML = '';
+        tbody.innerHTML = ''; // Clear the table body
+        thead.innerHTML = ''; // Clear the table header
 
         if (Object.keys(results).length == 0) {
-            return;
+            return; // If there are no results, exit the function
         }
 
+        // Create table header row
         const theadrow = document.createElement('tr');
         if (buttons != undefined) {
+            // If buttons are provided, create a header cell for them
             const buttonHeaderCell = document.createElement('th');
             theadrow.appendChild(buttonHeaderCell);
         }
+        // Create header cells for each column in the results
         Object.entries(results[0]).forEach(([key, value]) => {
             if (key !== 'ID' && key !== 'DOC_NUM' && key !== 'ROUTE_NUM') {
                 const headerCell = document.createElement('th');
@@ -24,219 +34,156 @@ class JsFunctions {
             }
         });
 
-        thead.appendChild(theadrow);
+        thead.appendChild(theadrow); // Add the header row to the table header
 
+        // Loop through each result item and create table rows
         Object.entries(results).forEach(([key, item]) => {
-            const row = document.createElement('tr');
-            var found = 0;
+            const row = document.createElement('tr'); // Create a table row for each result item
+            var found = 0; // Variable to track if filter text is found in the row
 
             if (buttons != undefined) {
+                // If buttons are provided, create a cell for them
                 const buttonCell = document.createElement('td');
                 const div = document.createElement('div');
                 div.className = "btn_container";
 
                 buttons.forEach(btn => {
+                    // Create button element for each button provided
                     const button = document.createElement('button');
-                    // button.innerText = btn.label;
-                    button.classList.add(btn.className);
-                    if (btn.className == "btnS") {
-                        button.classList.add('fa-regular', 'fa-paper-plane');
-                        button.title = "Send";
-                    } if (btn.className == "btnR") {
-                        button.classList.add('fa-solid', 'fa-inbox');
-                        button.title = "Receive";
-                    }
-                    if (btn.className == "btnA") {
-                        button.classList.add('fa-solid', 'fa-paperclip');
-                        button.title = "Attach";
-                    }
-                    if (btn.className == "btnE") {
-                        button.classList.add('fa-solid', 'fa-pen-to-square');
-                        button.title = "Edit";
-                    }
-                    if (btn.className == "btnCR" || btn.className == "btnCS") {
-                        button.classList.add('fa-solid', 'fa-ban');
-                        button.title = "Cancel";
-                    }
-                    if (btn.className == "btnT") {
-                        button.classList.add('fa-solid', 'fa-location-pin');
-                        button.title = "Tracking";
-                    }
-                    if (btn.className == "btnP") {
-                        button.classList.add('fa-solid', 'fa-print');
-                        button.title = "Print Routing Slip";
-                    }
-
-
-
-                    // button.className = btn.className;
-                    // button.id = `button-${item.ID}-${item.DOC_NUM}-${item.ROUTE_NUM}`;
+                    button.classList.add(btn.className); // Add CSS class to the button
+                    button.title = btn.title; // Set the tooltip title for the button
                     button.dataset.i = item.ID;
                     button.dataset.d = item.DOC_NUM;
                     button.dataset.r = item.ROUTE_NUM;
 
+                    // Add appropriate icon classes to the button based on its purpose
+                    // (e.g., Send, Receive, Attach, Edit, Cancel, Tracking, Print)
+                    if (btn.className == "btnS") {
+                        button.classList.add('fa-regular', 'fa-paper-plane');
+                    } else if (btn.className == "btnR") {
+                        button.classList.add('fa-solid', 'fa-inbox');
+                    } else if (btn.className == "btnA") {
+                        button.classList.add('fa-solid', 'fa-paperclip');
+                    } else if (btn.className == "btnE") {
+                        button.classList.add('fa-solid', 'fa-pen-to-square');
+                    } else if (btn.className == "btnCR" || btn.className == "btnCS") {
+                        button.classList.add('fa-solid', 'fa-ban');
+                    } else if (btn.className == "btnT") {
+                        button.classList.add('fa-solid', 'fa-location-pin');
+                    } else if (btn.className == "btnP") {
+                        button.classList.add('fa-solid', 'fa-print');
+                    }
+
+                    // Set the button's dataset attributes and disable based on certain conditions
+                    // (e.g., Disable buttons for specific actions)
                     if (item.Action == "RECEIVED" && btn.className == "btnR") {
                         button.disabled = true;
-                        // button.style.visibility = 'hidden';
-                    }
-                    if (item.Action == "SENT" && btn.className == "btnCR") {
+                    } else if (item.Action == "SENT" && btn.className == "btnCR") {
                         button.disabled = true;
-                        // button.style.visibility = 'hidden';
-                    }
-                    if (item.Action == "RECEIVED" && btn.className == "btnCS") {
+                    } else if (item.Action == "RECEIVED" && btn.className == "btnCS") {
                         button.disabled = true;
-                        // button.style.visibility = 'hidden';
-                    }
-                    if (item.Action == "CANCELLED") {
+                    } else if (item.Action == "CANCELLED") {
                         button.disabled = true;
-                        // button.style.visibility = 'hidden';
                     }
 
-                    div.appendChild(button);
-
+                    div.appendChild(button); // Append the button to the button container
                 });
 
-                row.appendChild(div);
+                row.appendChild(div); // Add the button cell to the row
             }
 
+            // Loop through each item property and create table cells
             Object.entries(item).forEach(([key, value]) => {
                 if (key !== 'ID' && key !== 'DOC_NUM' && key !== 'ROUTE_NUM') {
-                    const cell = document.createElement('td');
-                    var fvalue = value;
+                    const cell = document.createElement('td'); // Create a table cell
+                    var fvalue = value; // Store the value to be displayed in the cell
 
+                    // Customize cell styles based on the cell value (e.g., for different statuses)
                     if (fvalue == "RECEIVED") {
                         cell.style.backgroundColor = "#006eff";
                         cell.style.color = "#ffffff";
                         cell.style.fontSize = "15px";
-                    }
-
-                    if (fvalue == "SENT") {
+                    } else if (fvalue == "SENT") {
                         cell.style.backgroundColor = "#23d100";
                         cell.style.color = "#ffffff";
                         cell.style.fontSize = "15px";
-                    }
-
-                    if (fvalue == "CANCELLED") {
+                    } else if (fvalue == "CANCELLED") {
                         cell.style.backgroundColor = "#ffa600";
                         cell.style.color = "#ffffff";
                         cell.style.fontSize = "15px";
-                        // row.style.display = "none";
-                    }
-
-                    if (fvalue == "EDITED") {
+                    } else if (fvalue == "EDITED") {
                         cell.style.backgroundColor = "#e736ff";
                         cell.style.color = "#ffffff";
                         cell.style.fontSize = "15px";
-                    }
-
-                    if (fvalue == "DUPLICATED") {
+                    } else if (fvalue == "DUPLICATED") {
                         cell.style.backgroundColor = "#790000";
                         cell.style.color = "#ffffff";
                         cell.style.fontSize = "15px";
                     }
 
-                    cell.textContent = fvalue;
-                    row.appendChild(cell);
+                    cell.textContent = fvalue; // Set the cell text content
+                    row.appendChild(cell); // Add the cell to the row
 
+                    // Check if the cell text content matches the filter text
                     if (cell.textContent.toUpperCase().indexOf(filter) > -1) {
-                        found++;
+                        found++; // Increment the found counter
                     }
-
                 }
             });
 
+            // Hide the row if no matches were found based on the filter text
             if (found == 0) {
                 row.style.display = "none";
             }
 
-            tbody.appendChild(row);
+            tbody.appendChild(row); // Add the row to the table body
         });
 
-        // for (const key in results) {
-        //     if (results.hasOwnProperty(key)) {
-        //         const item = results[key];
-        //         for(const value in item){
-        //             console.log(value);
-        //         }
-        //     }
-        // }
-
-        // const thead = table.querySelector('thead');
-        // const tbody = table.querySelector('tbody');
-        // const rows = tbody.querySelectorAll('tr');
-        // for (var i = 0; i < rows.length; i++) {
-        //     var td = rows[i].querySelectorAll("td");
-        //     var found = false;
-        //     for (var j = 0; j < td.length; j++) {
-        //         if (td[j]) {
-        //             var txtValue = td[j].textContent || td[j].innerText;
-        //             if (txtValue.toUpperCase().indexOf(filter) > -1) {
-        //                 found = true;
-        //                 break;
-        //             }
-        //         }
-        //     }
-        //     if (found) {
-        //         rows[i].style.display = "";
-        //     } else {
-        //         rows[i].style.display = "none";
-        //     }
-        // }
-
-        thead.append(theadrow);
-
+        thead.append(theadrow); // Add the header row to the table header
     }
-    // static updateAttachments(mini, results, buttons) {
-    //     console.log(results);
-    //     mini.innerHTML = '';
-    //     Object.entries(results).forEach(([key, item]) => {
-    //         const doc = document.createElement("div");
-    //         doc.innerText = item['DESCRIPTION'];
-    //         doc.className = "atc_items";
-    //         doc.addEventListener('click', function () {
-    //             getData(_REQUEST.GET_ATTACHMENT, { ID: item.ID }, (result) => {
-    //                 const url = "../../" + result;
-    //                 window.open("../../RESOURCES/pdfJS/web/viewer.html?file=" + url, '_blank').focus();
-    //             }, null);
-    //         });
 
-    //         // doc.addEventListener('mouseout', function () {
-    //         //     preview.style.backgroundImage = "";
-    //         // });
-    //         mini.appendChild(doc);
-
-    //     });
-    // }
+    /**
+    * Converts form data to a JSON object.
+    * @param {HTMLFormElement} form - The HTML form element to convert.
+    * @returns {Object} - The JSON object representing the form data.
+    */
     static FormToJson(form) {
+        // Create a FormData object from the form
         var formData = new FormData(form);
+
+        // Initialize an empty object to store the form data
         var formDataObject = {};
+
+        // Iterate through each entry in the FormData object
         formData.forEach(function (value, key) {
+            // Assign each form field value to its corresponding key in the object
             formDataObject[key] = value;
         });
+
+        // Return the JSON object containing the form data
         return formDataObject;
     }
 
+    /**
+    * Sets options for a <select> element.
+    * @param {HTMLSelectElement} element - The <select> element to set options for.
+    * @param {Object} options - An object containing key-value pairs representing option values and labels.
+    */
     static setSelect(element, options) {
-        // console.log(options);
-        // for (let i = 0; i < options.length; i++) {
-        //     var option = document.createElement('option');
-        //     var somedata = Object.values(options[i]);
-
-        //     option.value = somedata[0];
-        //     option.innerText = somedata[1];
-
-        //     element.appendChild(option);
-        // }
-
+        // Iterate through each key-value pair in the options object
         for (var key in options) {
+            // Create a new <option> element
             var option = document.createElement('option');
 
+            // Set the value and text content of the <option> element
             option.value = key;
             option.textContent = options[key];
 
+            // Append the <option> element to the <select> element
             element.appendChild(option);
         }
     }
+
     static checkIfEmpty(values) {
         var empty = false;
         for (let i = 0; i < values.length; i++) {
