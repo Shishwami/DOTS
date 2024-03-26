b2515b1fbbeddd7f74c031b10431f0d1ef83ffd9
Shishwami <157940784+Shishwami@users.noreply.github.com>
2024-03-26 14:36:24 +0800

    Update logout.php

--- SCRIPTS/logout.php
+++ SCRIPTS/logout.php
@@ -1,17 +1,20 @@
 <?php
 
+// Check if a session is active, and if not, start a new session
 if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
 
-// Unset all of the session variables
+// Unset all of the session variables by assigning an empty array to $_SESSION
 $_SESSION = array();
 
-// Destroy the session
+// Destroy the session data on the server
 session_destroy();
 
-// Redirect to the login page
+// Redirect the user to the login page (assuming the login page is located at '../index.php')
 header("Location: ../index.php");
+
+// Exit the script to prevent further execution
 exit;
 
 ?>
